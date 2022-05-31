054c58fb814c8845076f00b310bcfc2b82b7a5db
Dan M <dan@green-coding.org>
2022-05-31 17:40:32 +0200

    added stripped down and modified rapl-read file and built object. outputs system time (nanoseconds) and main energy package reading from msr, only

---
+++ tools/rapl-read.c
@@ -0,0 +1,411 @@
+/* Read the RAPL registers on recent (>sandybridge) Intel processors	*/
+/*									*/
+/* There are currently three ways to do this:				*/
+/*	1. Read the MSRs directly with /dev/cpu/??/msr			*/
+/*	2. Use the perf_event_open() interface				*/
+/*	3. Read the values from the sysfs powercap interface		*/
+/*									*/
+/* MSR Code originally based on a (never made it upstream) linux-kernel	*/
+/*	RAPL driver by Zhang Rui <rui.zhang@intel.com>			*/
+/*	https://lkml.org/lkml/2011/5/26/93				*/
+/* Additional contributions by:						*/
+/*	Romain Dolbeau -- romain @ dolbeau.org				*/
+/*									*/
+/* For raw MSR access the /dev/cpu/??/msr driver must be enabled and	*/
+/*	permissions set to allow read access.				*/
+/*	You might need to "modprobe msr" before it will work.		*/
+/*									*/
+/* perf_event_open() support requires at least Linux 3.14 and to have	*/
+/*	/proc/sys/kernel/perf_event_paranoid < 1			*/
+/*									*/
+/* the sysfs powercap interface got into the kernel in 			*/
+/*	2d281d8196e38dd (3.13)						*/
+/*									*/
+/* Compile with:   gcc -O2 -Wall -o rapl-read rapl-read.c -lm		*/
+/*									*/
+/* Vince Weaver -- vincent.weaver @ maine.edu -- 11 September 2015	*/
+/*									*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+
+
+/* AMD Support */
+#define MSR_AMD_RAPL_POWER_UNIT			0xc0010299
+
+#define MSR_AMD_PKG_ENERGY_STATUS		0xc001029B
+#define MSR_AMD_PP0_ENERGY_STATUS		0xc001029A
+
+
+
+/* Intel support */
+
+#define MSR_INTEL_RAPL_POWER_UNIT		0x606
+/*
+ * Platform specific RAPL Domains.
+ * Note that PP1 RAPL Domain is supported on 062A only
+ * And DRAM RAPL Domain is supported on 062D only
+ */
+/* Package RAPL Domain */
+#define MSR_PKG_RAPL_POWER_LIMIT	0x610
+#define MSR_INTEL_PKG_ENERGY_STATUS	0x611
+#define MSR_PKG_PERF_STATUS		0x613
+#define MSR_PKG_POWER_INFO		0x614
+
+/* PP0 RAPL Domain */
+#define MSR_PP0_POWER_LIMIT		0x638
+#define MSR_INTEL_PP0_ENERGY_STATUS	0x639
+#define MSR_PP0_POLICY			0x63A
+#define MSR_PP0_PERF_STATUS		0x63B
+
+/* PP1 RAPL Domain, may reflect to uncore devices */
+#define MSR_PP1_POWER_LIMIT		0x640
+#define MSR_PP1_ENERGY_STATUS		0x641
+#define MSR_PP1_POLICY			0x642
+
+/* DRAM RAPL Domain */
+#define MSR_DRAM_POWER_LIMIT		0x618
+#define MSR_DRAM_ENERGY_STATUS		0x619
+#define MSR_DRAM_PERF_STATUS		0x61B
+#define MSR_DRAM_POWER_INFO		0x61C
+
+/* PSYS RAPL Domain */
+#define MSR_PLATFORM_ENERGY_STATUS	0x64d
+
+/* RAPL UNIT BITMASK */
+#define POWER_UNIT_OFFSET	0
+#define POWER_UNIT_MASK		0x0F
+
+#define ENERGY_UNIT_OFFSET	0x08
+#define ENERGY_UNIT_MASK	0x1F00
+
+#define TIME_UNIT_OFFSET	0x10
+#define TIME_UNIT_MASK		0xF000
+
+static int open_msr(int core) {
+
+	char msr_filename[BUFSIZ];
+	int fd;
+
+	sprintf(msr_filename, "/dev/cpu/%d/msr", core);
+	fd = open(msr_filename, O_RDONLY);
+	if ( fd < 0 ) {
+		if ( errno == ENXIO ) {
+			fprintf(stderr, "rdmsr: No CPU %d\n", core);
+			exit(2);
+		} else if ( errno == EIO ) {
+			fprintf(stderr, "rdmsr: CPU %d doesn't support MSRs\n",
+					core);
+			exit(3);
+		} else {
+			perror("rdmsr:open");
+			exit(127);
+		}
+	}
+
+	return fd;
+}
+
+static long long read_msr(int fd, unsigned int which) {
+
+	uint64_t data;
+
+	if ( pread(fd, &data, sizeof data, which) != sizeof data ) {
+		perror("rdmsr:pread");
+		fprintf(stderr,"Error reading MSR %x\n",which);
+		exit(127);
+	}
+
+	return (long long)data;
+}
+
+#define CPU_VENDOR_INTEL	1
+#define CPU_VENDOR_AMD		2
+
+#define CPU_SANDYBRIDGE		42
+#define CPU_SANDYBRIDGE_EP	45
+#define CPU_IVYBRIDGE		58
+#define CPU_IVYBRIDGE_EP	62
+#define CPU_HASWELL		60
+#define CPU_HASWELL_ULT		69
+#define CPU_HASWELL_GT3E	70
+#define CPU_HASWELL_EP		63
+#define CPU_BROADWELL		61
+#define CPU_BROADWELL_GT3E	71
+#define CPU_BROADWELL_EP	79
+#define CPU_BROADWELL_DE	86
+#define CPU_SKYLAKE		78
+#define CPU_SKYLAKE_HS		94
+#define CPU_SKYLAKE_X		85
+#define CPU_KNIGHTS_LANDING	87
+#define CPU_KNIGHTS_MILL	133
+#define CPU_KABYLAKE_MOBILE	142
+#define CPU_KABYLAKE		158
+#define CPU_ATOM_SILVERMONT	55
+#define CPU_ATOM_AIRMONT	76
+#define CPU_ATOM_MERRIFIELD	74
+#define CPU_ATOM_MOOREFIELD	90
+#define CPU_ATOM_GOLDMONT	92
+#define CPU_ATOM_GEMINI_LAKE	122
+#define CPU_ATOM_DENVERTON	95
+#define CPU_TIGER_LAKE		140
+
+
+#define CPU_AMD_FAM17H		0xc000
+
+static unsigned int msr_rapl_units,msr_pkg_energy_status,msr_pp0_energy_status;
+
+static int detect_cpu(void) {
+
+	FILE *fff;
+
+	int vendor=-1,family,model=-1;
+	char buffer[BUFSIZ],*result;
+	char vendor_string[BUFSIZ];
+
+	fff=fopen("/proc/cpuinfo","r");
+	if (fff==NULL) return -1;
+
+	while(1) {
+		result=fgets(buffer,BUFSIZ,fff);
+		if (result==NULL) break;
+
+		if (!strncmp(result,"vendor_id",8)) {
+			sscanf(result,"%*s%*s%s",vendor_string);
+
+			if (!strncmp(vendor_string,"GenuineIntel",12)) {
+				vendor=CPU_VENDOR_INTEL;
+			}
+			if (!strncmp(vendor_string,"AuthenticAMD",12)) {
+				vendor=CPU_VENDOR_AMD;
+			}
+		}
+
+		if (!strncmp(result,"cpu family",10)) {
+			sscanf(result,"%*s%*s%*s%d",&family);
+		}
+
+		if (!strncmp(result,"model",5)) {
+			sscanf(result,"%*s%*s%d",&model);
+		}
+
+	}
+
+	if (vendor==CPU_VENDOR_INTEL) {
+		if (family!=6) {
+			printf("Wrong CPU family %d\n",family);
+			return -1;
+		}
+
+		msr_rapl_units=MSR_INTEL_RAPL_POWER_UNIT;
+		msr_pkg_energy_status=MSR_INTEL_PKG_ENERGY_STATUS;
+		msr_pp0_energy_status=MSR_INTEL_PP0_ENERGY_STATUS;
+	}
+
+	if (vendor==CPU_VENDOR_AMD) {
+
+		msr_rapl_units=MSR_AMD_RAPL_POWER_UNIT;
+		msr_pkg_energy_status=MSR_AMD_PKG_ENERGY_STATUS;
+		msr_pp0_energy_status=MSR_AMD_PP0_ENERGY_STATUS;
+
+		if (family!=23) {
+			printf("Wrong CPU family %d\n",family);
+			return -1;
+		}
+		model=CPU_AMD_FAM17H;
+	}
+
+	fclose(fff);
+
+	return model;
+}
+
+#define MAX_CPUS	1024
+#define MAX_PACKAGES	16
+
+static int total_cores=0,total_packages=0;
+static int package_map[MAX_PACKAGES];
+
+static int detect_packages(void) {
+
+	char filename[BUFSIZ];
+	FILE *fff;
+	int package;
+	int i;
+
+	for(i=0;i<MAX_PACKAGES;i++) package_map[i]=-1;
+
+	for(i=0;i<MAX_CPUS;i++) {
+		sprintf(filename,"/sys/devices/system/cpu/cpu%d/topology/physical_package_id",i);
+		fff=fopen(filename,"r");
+		if (fff==NULL) break;
+		fscanf(fff,"%d",&package);
+		fclose(fff);
+
+		if (package_map[package]==-1) {
+			total_packages++;
+			package_map[package]=i;
+		}
+
+	}
+
+	total_cores=i;
+	return 0;
+}
+
+static double sleep_time=1;
+
+/*******************************/
+/* MSR code                    */
+/*******************************/
+
+static int rapl_msr(int core, int cpu_model) {
+
+	int fd;
+	long long result;
+	double power_units,time_units;
+	double cpu_energy_units[MAX_PACKAGES];
+	double package_before[MAX_PACKAGES],package_after[MAX_PACKAGES];
+	int j;
+	struct timeval now;
+
+
+	if (cpu_model<0) {
+		printf("\tUnsupported CPU model %d\n",cpu_model);
+		return -1;
+	}
+
+	for(j=0;j<total_packages;j++) {
+		fd=open_msr(package_map[j]);
+
+		/* Calculate the units used */
+		result=read_msr(fd,msr_rapl_units);
+
+		power_units=pow(0.5,(double)(result&0xf));
+
+		cpu_energy_units[j]=pow(0.5,(double)((result>>8)&0x1f));
+
+		time_units=pow(0.5,(double)((result>>16)&0xf));
+
+		close(fd);
+
+	}
+
+	for(j=0;j<total_packages;j++) {
+
+		fd=open_msr(package_map[j]);
+
+		/* Package Energy */
+		result=read_msr(fd,msr_pkg_energy_status);
+		package_before[j]=(double)result*cpu_energy_units[j];
+
+		close(fd);
+	}
+
+	sleep(sleep_time);
+
+	for(j=0;j<total_packages;j++) {
+		fd=open_msr(package_map[j]);
+
+		result=read_msr(fd,msr_pkg_energy_status);
+		package_after[j]=(double)result*cpu_energy_units[j];
+
+		float energy_output = package_after[j]-package_before[j];
+		if (energy_output != 0.0) {
+			gettimeofday(&now, NULL);
+			printf("%ld%ld %.9f\n", now.tv_sec, now.tv_usec, energy_output);
+		}
+
+		close(fd);
+	}
+
+	return 0;
+}
+
+
+#define NUM_RAPL_DOMAINS	5
+
+char rapl_domain_names[NUM_RAPL_DOMAINS][30]= {
+	"energy-cores",
+	"energy-gpu",
+	"energy-pkg",
+	"energy-ram",
+	"energy-psys",
+};
+
+
+int main(int argc, char **argv) {
+
+	int c;
+	int core=0;
+	int result=-1;
+	int cpu_model;
+	int time=-1;
+	opterr=0;
+
+	while ((c = getopt (argc, argv, "c:ht:i:")) != -1) {
+		switch (c) {
+		case 'c':
+			core = atoi(optarg);
+			break;
+		case 'h':
+			printf("Usage: %s [-c core] [-h] [-m]\n\n",argv[0]);
+			printf("\t-c core : specifies which core to measure\n");
+			printf("\t-h      : displays this help\n");
+			printf("\t-t      : specifies how many seconds to run for. Will run indefinitely if left empty\n");
+			printf("\t-i      : specifies the interval that will be slept between measurements");
+			exit(0);
+		case 't':
+			time = atoi(optarg);
+			break;
+		case 'i':
+			sleep_time = atoi(optarg);
+			break;
+		default:
+			fprintf(stderr,"Unknown option %c\n",c);
+			exit(-1);
+		}
+	}
+
+	cpu_model=detect_cpu();
+	detect_packages();
+
+	if (time<0) {
+		while(1) {
+			result=rapl_msr(core,cpu_model);	
+		}
+	}
+	else {
+		int i;
+		for(i=0;i<time;i++) {
+			result=rapl_msr(core,cpu_model);
+		}
+
+	}
+
+	result=rapl_msr(core,cpu_model);
+
+	if (result<0) {
+
+		printf("Unable to read RAPL counters.\n");
+		printf("* Verify you have an Intel Sandybridge or newer processor\n");
+		printf("* You may need to run as root or have /proc/sys/kernel/perf_event_paranoid set properly\n");
+		printf("* Make sure msr module is installed\n");
+		printf("\n");
+
+		return -1;
+
+	}
+
+	return 0;
+}
