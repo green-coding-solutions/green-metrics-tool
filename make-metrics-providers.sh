ca091078c07cd03a0c9742caad30e14c1c60a909
Dan M <dan@green-coding.org>
2022-06-27 15:20:43 +0200

    removed unnecessary line

--- make-metrics-providers.sh
+++ make-metrics-providers.sh
@@ -3,7 +3,6 @@ parent_dir="./tools/metric-providers"
 make_file="Makefile"
 find "$parent_dir" -type d |
 while IFS= read -r subdir; do
-    all_present=true
     make_path="$subdir/$make_file"
     if [[ -f "$make_path" ]]; then
         rm $subdir/static-binary
