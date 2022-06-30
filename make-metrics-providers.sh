add3da69c191db625c430fe7ad0c48ce101976e6
Dan M <dan@green-coding.org>
2022-06-30 11:51:30 +0200

    wip

--- make-metrics-providers.sh
+++ make-metrics-providers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-parent_dir="./tools/metric-providers"
+metrics_subdir="tools/metric-providers"
+parent_dir="./$metrics_subdir"
 make_file="Makefile"
 find "$parent_dir" -type d |
 while IFS= read -r subdir; do
@@ -7,5 +8,16 @@ while IFS= read -r subdir; do
     if [[ -f "$make_path" ]]; then
         rm $subdir/static-binary
         make -C $subdir
+        chmod +x $subdir/static-binary
+        if [[ "$subdir" == *"rapl/system/MSR"* ]] ; then
+            echo $USER
+            whoami
+            sudo_line="$USER    ALL=(ALL) NOPASSWD: /usr/bin/stdbuf -oL $PWD/$metrics_subdir/static-binary -i 1000"
+            if grep -Fxq "$sudo_line" /etc/sudoers ; then
+                echo $USER
+                # echo $USER
+                # whoami
+            fi
+        fi
     fi
 done
