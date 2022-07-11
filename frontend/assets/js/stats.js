27cb08510b732fd3515d8974a9a6c17c705a0253
Alexandre <alexandrebo@gmail.com>
2022-07-11 14:43:00 +0200

    add twoCards div to every pair of charts

--- frontend/assets/js/stats.js
+++ frontend/assets/js/stats.js
@@ -1,43 +1,62 @@
-function createChartContainer(scaffold, container, chart, options) {
-    const chart_node = document.querySelector(scaffold).cloneNode(true)
-    chart_node.style.display = "block";
-    document.querySelector(container).appendChild(chart_node);
-    return chart_node;
+function createChartContainer(scaffold, container, chart, el, counter) {
+  console.log("counter", counter)
+  const chart_node = document.querySelector(scaffold).cloneNode(true);
+  chart_node.style.display = "block";
+  chart_node.id = el;
+  
+  if (counter % 2 === 0) {
+    const twoCards = document.createElement("div");
+    twoCards.classList.add("ui");
+    twoCards.classList.add("two");
+    twoCards.classList.add("cards");
+    twoCards.classList.add("stackable");
+    const id = "twoCards" + counter;
+    twoCards.id = id;
+    document.querySelector(container).appendChild(twoCards);
+    document.getElementById(id).appendChild(chart_node);
+    console.log("created twoCards div")
+  } else {
+    const id = "twoCards" + (counter - 1);
+    console.log("belongs to already created div with id", id)
+    document.getElementById(id).appendChild(chart_node);
+  }
+
+  return chart_node;
 }
 
 function buildOptions(series, annotation, chart_title) {
-    const options = {
-        series: Object.values(series),
-        chart: {
-            type: 'area',
-            animations: {
-              enabled: false
-            }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {curve: 'smooth'},
-        tooltip: {
-            x: { format: 'dd/MM/yy HH:mm'},
-        },
-        xaxis: { tickAmount: 6, type: "datetime"},
-        annotations: { xaxis: annotation },
-        title: {text: chart_title}
-    };
+  const options = {
+    series: Object.values(series),
+    chart: {
+      type: "area",
+      animations: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: { curve: "smooth" },
+    tooltip: {
+      x: { format: "dd/MM/yy HH:mm" },
+    },
+    xaxis: { tickAmount: 6, type: "datetime" },
+    annotations: { xaxis: annotation },
+    title: { text: chart_title },
+  };
 
-    return options;
+  return options;
 }
-
 
 const toggleNotes = () => {
-  const notes = document.getElementsByClassName('dygraph-annotation');
-  console.log("notes", notes)
+  const notes = document.getElementsByClassName("dygraph-annotation");
+  console.log("notes", notes);
   for (let i = 0; i < notes.length; i++) {
-    if (!notes[i].style.display || notes[i].style.display === "block") notes[i].style.display = "none";
+    if (!notes[i].style.display || notes[i].style.display === "block")
+      notes[i].style.display = "none";
     else notes[i].style.display = "block";
   }
-}
+};
 
 const formatData = (totalContainers, currentContainer, X, Y) => {
   let arr = [X];
@@ -46,7 +65,7 @@ const formatData = (totalContainers, currentContainer, X, Y) => {
     else arr.push(NaN);
   }
   return arr;
-}
+};
 
 const getDataAndLabels = (series) => {
   let containerX;
@@ -63,45 +82,42 @@ const getDataAndLabels = (series) => {
     }
   }
   return { data, labels };
-}
+};
 
 const createGraph = (element, data, labels, title) => {
-  return new Dygraph(element,
-                        data,
-                        {
-                          labels,
-                          fillGraph: true,
-                          rollPeriod: 10,
-                          showRoller: true,
-                          title,
-                                legend: 'always' ,
-                                labelsSeparateLines: true,
-                          highlightSeriesOpts: { strokeWidth: 2 },
-                          // showLabelsOnHighlight: false,
-                          axes: {
-                            x: {
-                              axisLabelFormatter: Dygraph.dateAxisLabelFormatter,
-                              ticker: Dygraph.dateTicker,
-                            }
-                          },
-                          drawCallback: function(g) {
-                            // const notes = document.getElementsByClassName('dygraph-annotation');
-                            // for (let i = 0; i < notes.length; i++) {
-                            //   if (notes[i].style.top === "") notes[i].style.display = "none";
-                            // }
-                          },
-                          annotationMouseOverHandler: function(ann, point, dg, event) {
-                            let modal = document.getElementById("myModal");
-                            modal.style.display = "block";
-                            modal.innerHTML = ` <!-- Modal content -->
+  return new Dygraph(element, data, {
+    labels,
+    fillGraph: true,
+    rollPeriod: 10,
+    showRoller: true,
+    title,
+    legend: "always",
+    labelsSeparateLines: true,
+    highlightSeriesOpts: { strokeWidth: 2 },
+    // showLabelsOnHighlight: false,
+    axes: {
+      x: {
+        axisLabelFormatter: Dygraph.dateAxisLabelFormatter,
+        ticker: Dygraph.dateTicker,
+      },
+    },
+    drawCallback: function (g) {
+      // const notes = document.getElementsByClassName('dygraph-annotation');
+      // for (let i = 0; i < notes.length; i++) {
+      //   if (notes[i].style.top === "") notes[i].style.display = "none";
+      // }
+    },
+    annotationMouseOverHandler: function (ann, point, dg, event) {
+      let modal = document.getElementById("myModal");
+      modal.style.display = "block";
+      modal.innerHTML = ` <!-- Modal content -->
                                                 <div class="modal-content">
                                                   <p>${ann.text}</p>
-                                                </div>`
-                          },
-                          annotationMouseOutHandler: function(ann, point, dg, event) {
-                            let modal = document.getElementById("myModal");
-                            modal.style.display = "none";
-                          },
-                        });
-}
-
+                                                </div>`;
+    },
+    annotationMouseOutHandler: function (ann, point, dg, event) {
+      let modal = document.getElementById("myModal");
+      modal.style.display = "none";
+    },
+  });
+};
