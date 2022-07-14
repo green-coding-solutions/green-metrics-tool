82e79d839a9233fbbf827aa59cfd41f3a7c4c50a
Arne Tarara <arne@datafuse.de>
2022-07-14 23:31:32 +0200

    Initial cleaning

---
+++ frontend/js/index.js
@@ -0,0 +1,45 @@
+(() => {
+    let content = [];
+    makeAPICall('/v1/projects', (my_json) => {
+        my_json.data.forEach(el => {
+
+            const li_node = document.createElement("tr");
+            const link_node = document.createElement('a');
+            content.push({ title: el[1] });
+            link_node.innerText = el[1];
+            link_node.title = el[1];
+            link_node.href = "/stats.html?id=" + el[0];
+            li_node.appendChild(link_node);
+            li_node.innerHTML = '<td class="td-index">' + li_node.innerHTML + '</td><td class="td-index">' + el[2] + '</td><td class="td-index">' + el[3] + '</td><td><input type="checkbox" value="' + el[0] + '" name="chbx-proj" />&nbsp;</td>';
+            document.getElementById("projects-table").appendChild(li_node);
+        });
+        $('.ui.search').search({ source: content });
+    });
+
+    const compareButton = () => {
+        var checkedBoxes = document.querySelectorAll('input[name=chbx-proj]:checked');
+        console.log(checkedBoxes.length);
+        var link = "";
+        if (checkedBoxes.length == 2) {
+            link = "/multi.html?dummy=dummy";
+        }
+        else if (checkedBoxes.length > 2) {
+            link = "/compare.html?dummy=dummy";
+        }
+        else {
+            alert("Please select at least two projects to compare :-)");
+            return;
+        }
+
+        checkedBoxes.forEach(checkbox => {
+            link += "&pid[]=" + checkbox.value;
+        });
+        //console.log(link);
+        window.location = link;
+    }
+
+    $(document).ready(function () {
+        $('table').tablesort();
+    });
+
+})();
---
+++ frontend/js/main.js
@@ -0,0 +1,42 @@
+
+const makeAPICall = (path, callback) => {
+    try {
+        if (document.location.host.indexOf('metrics.green-coding.org') === 0)
+            api_url = "https://api.green-coding.org";
+        else
+            api_url = "http://api.green-coding.local:8000";
+
+        fetch(api_url + path)
+            .then(response => response.json())
+            .then(my_json => {
+                if (my_json.success != true) {
+                    alert(my_json.err);
+                    return;
+                }
+                callback(my_json);
+            })
+    } catch (e) {
+        alert("Fetch failed: " + e)
+    }
+};
+
+(() => {
+    /* Menu toggling */
+    let openMenu = function(e){
+        $(this).removeClass('closed').addClass('opened');
+        $(this).find('i').removeClass('right').addClass('left');
+        $('#menu').removeClass('collapsed');
+        $('#main').removeClass('collapsed');
+
+    }
+    $(document).on('click','#menu-toggle.closed', openMenu);
+
+    let closeMenu = function(e){
+        $(this).removeClass('opened').addClass('closed');
+        $(this).find('i').removeClass('left').addClass('right');
+        $('#menu').addClass('collapsed');
+        $('#main').addClass('collapsed');
+    }
+
+    $(document).on('click','#menu-toggle.opened', closeMenu);
+})();
