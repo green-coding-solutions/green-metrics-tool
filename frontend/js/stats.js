ef817c7ceace941a8611811d63dd0b8fa5cd6b90
Arne Tarara <arne@datafuse.de>
2022-07-16 11:00:32 +0200

    More stats updated

--- frontend/js/stats.js
+++ frontend/js/stats.js
@@ -1,32 +1,54 @@
+const getApexOptions = () => {
+    return {
+        series: null,
+        chart: {
+            type: 'area',
+            animations: {
+              enabled: false
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        title: {
+          text: '',
+        },
+        xaxis: {
+          tickAmount: 6,
+          type: "datetime"
+        },
+        annotations: {
+            xaxis: []
+        },
+        tooltip: {
+            enabled: true,
+            shared: true,
+            followCursor: true,
+            x: {
+              show: true,
+              format: 'HH:mm:ss',
+          },
+
+        },
+    };
+}
 
 
-const getAnnotations = (my_json) => {
-    console.log("my_json from the notes", my_json)
+const getEChartsOptions = () => {
+    return {
+        tooltip: {
+            trigger: 'axis'
+          },
 
-  //   const replicateAnnotations = (target) => {
-  //     if (el[2] !== target) { // avoids duplication of annotations on the same chart
-  //         if (my_series[target][el[0]] == undefined) {
-  //             my_series[target][el[0]] = { name: el[0], data: [{ x: el[1], y: 0 }] }
-  //         } else {
-  //             my_series[target][el[0]]['data'].push({ x: el[1], y: 0 })
-  //         }
-  //     }
-  // }
-
-  // for (let i = 0; i < my_json.data.length; i++) {
-  //     if (my_json.data[i][1] !== "[SYSTEM]") {
-  //         annotations.push({
-  //             series: my_json.data[i][1],
-  //             x: my_json.data[i][3] / 1000,
-  //       shortText: my_json.data[i][2][0] === " " ? my_json.data[i][2][1] : my_json.data[i][2][0], // first letter of the message; in case it's a space, then seccond letter
-  //       text: my_json.data[i][2],
-  //   })
-  //     }
-  // }
-  // hardcoding annotations' timestamps so that they appear on the graph
-  //annotations[0].x = 1657294498387.516;
-  //annotations[1].x = 1657294500298.983;
-  //console.log("annotations", annotations)
+      xAxis: [],
+      yAxis: {
+        type: 'value'
+      },
+      series: []
+    };
 }
 
 const fillAvgContainers = (project) => {
@@ -41,11 +63,9 @@ const fillAvgContainers = (project) => {
 
 }
 
-const getData = (my_json) => {
-    const cpu_load = [];
-    const mem_load = [];
-    const my_series = {}
-    let total_energy = null;
+const getMetrics = (my_json, style='apex') => {
+    const metrics = {cpu_load: [], mem_load: [], series: {}, total_energy: 0}
+
     let accumulate = 0;
 
 
@@ -56,41 +76,41 @@ const getData = (my_json) => {
         el[2] // metric name
         el[3] // value -> This value might need to be rescaled
         '*/
-
+        accumulate = 0; // default
         if (el[1] > my_json.project.start_measurement && el[1] < my_json.project.end_measurement) {
             accumulate = 1;
-        } else {
-            accumulate = 0;
         }
 
         let time_in_ms = el[1] / 1000; // divide nanoseconds timestamp to ms to be handled by charting lib
+        let value = el[3]; // default
 
-        let value = null;
-
-        if (el[2] == 'cpu_cgroup') {
-            if (accumulate === 1) cpu_load.push(el[3]);
+        if (el[2] == 'cpu_cgroup_container') {
+            if (accumulate === 1) metrics.cpu_load.push(el[3]);
             value = el[3] / 100; // CPU comes as ratio, but since stored as integer is was multiplicated with 100
-        } else if (el[2] == 'energy_system_RAPL_MSR') {
-            if (accumulate === 1) total_energy += el[3];
+        } else if (el[2] == 'energy_RAPL_MSR_system') {
+            if (accumulate === 1) metrics.total_energy += el[3];
             value = el[3];
-        } else if (el[2] == 'memory_cgroup') {
-            mem_load.push(el[3]);
+        } else if (el[2] == 'memory_cgroup_container') {
+            metrics.mem_load.push(el[3]);
             value = el[3] / 1000000; // make memory in MB
-        } else {
-            value = el[3]; // all other metrics we keep in same order of magnitude
         }
 
-
-        if (my_series[el[2]] == undefined)  my_series[el[2]] = {};
-
-        if (my_series[el[2]][el[0]] == undefined) {
-            my_series[el[2]][el[0]] = { name: el[0], data: [{ x: time_in_ms, y: value }] }
-        } else {
-            my_series[el[2]][el[0]]['data'].push({ x: time_in_ms, y: value })
+        // Depending on the charting library the object has to be reformatted
+        // First we check if structure is initialized
+        if (metrics.series[el[2]] == undefined)  metrics.series[el[2]] = {};
+        if (metrics.series[el[2]][el[0]] == undefined) {
+            metrics.series[el[2]][el[0]] = { name: el[0], time: [], data: [] }
         }
 
+        // now we handle the library specific formatting
+        if(style=='apex') {
+            metrics.series[el[2]][el[0]]['data'].push({ x: time_in_ms, y: value })
+        } else if(style=='echarts') {
+            metrics.series[el[2]][el[0]]['time'].push(time_in_ms)
+            metrics.series[el[2]][el[0]]['data'].push(value)
+        } else throw "Unknown chart style"
     })
-    return my_series;
+    return metrics;
 }
 
 const displayGraphs = (my_series, style='apex') => {
@@ -98,127 +118,40 @@ const displayGraphs = (my_series, style='apex') => {
   let counter = 0; // for automatically creating pair of <div>s
 
 
-    for ( el in my_series) {
-        const element = createChartContainer("#chart-container", el, counter)
+    for ( metric_name in my_series) {
+
+        const element = createChartContainer("#chart-container", metric_name, counter);
 
         if(style=='apex') {
-            charts = []
-
-            var options = {
-                series: Object.values(my_series[el]),
-                chart: {
-                    type: 'area',
-                    animations: {
-                      enabled: false
-                    }
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                stroke: {
-                    curve: 'smooth'
-                },
-                title: {
-                  text: el,
-                },
-                xaxis: {
-                  tickAmount: 6,
-                  type: "datetime"
-                },
-                annotations: {
-                    xaxis: []
-                },
-                tooltip: {
-                    enabled: true,
-                    shared: true,
-                    followCursor: true,
-                    x: {
-                      show: true,
-                      format: 'HH:mm:ss',
-                  },
-
-                },
-            };
-
-            (new ApexCharts(element, options)).render()
-        } else if(style == 'dygraphs') {
-            const { data, labels } = getFormattedDataAndLabels(Object.values(my_series[el]));
-            const chart = createGraph(element, data, labels, el)
-            //chart.setAnnotations(annotations);
+            charts = [];
+            let options = getApexOptions();
+            options.title.text = metric_name;
+            options.series = Object.values(my_series[metric_name]);
+            (new ApexCharts(element, options)).render();
         } else if(style == 'echarts') {
-            var myChart = echarts.init(element);
-            var option;
-            console.log(my_series)
-
-            option = {
-                tooltip: {
-                    trigger: 'axis'
-                  },
-
-              xAxis: [
-                {
-                    data: [1656410602411772, 1656410602421772, 1656410602711772, 1656410603411772, 1656410692411772, 1656410702411772, 1656410802411772]
-                },
-                {
-                    data: [1656410602412772, 1656410602499772, 1656410602711772, 1656410603411772, 1656410692411772, 1656410702411772, 1656410802413772]
-                },
-                {
-                    data: [1656410602422772, 1656410602433772, 1656410602755772, 1656410603411772, 1656410692411772, 1656410702411772, 1656410802413772]
-                },
-
-              ],
-              yAxis: {
-                type: 'value'
-              },
-              series: [
-                {
-                  data: [150, 230, 224, 218, 135, 147, 260],
-                  type: 'line'
-                },
-                {
-                  data: [13, 22, 700, 11, 135, 147, 260],
-                  type: 'line'
-                }
-
-              ]
-            };
-
-            myChart.setOption(option);
-
+            var options = getEChartsOptions();
+            for (container in my_series[metric_name]) {
+                options.xAxis.push({data: my_series[metric_name][container].time});
+                options.series.push({data: my_series[metric_name][container].data, type: 'line', areaStyle: {color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(55, 162, 255)'
+          },
+          {
+            offset: 1,
+            color: 'rgb(116, 21, 219)'
+          }
+        ])}});
+            }
+            echarts.init(element).setOption(options);
         } else {
-            throw "Unknown chart style"
+            throw "Unknown chart style";
         }
         counter++;
 
     }
 }
 
-const getFormattedDataAndLabels = (series) => {
-    let containerX;
-    let containerY;
-    let data = [];
-    let labels = ["Time"];
-
-    for (let i = 0; i < series.length; i++) {
-        labels.push(series[i].name || "");
-        for (let j = 0; j < series[i].data.length; j++) {
-            containerX = series[i].data[j].x;
-            containerY = series[i].data[j].y;
-            data.push(formatData(series.length, i, containerX, containerY));
-        }
-    }
-    console.log(data);
-    return { data, labels };
-};
-
-const formatData = (totalContainers, currentContainer, X, Y) => {
-    let arr = [X];
-    for (let i = 0; i < totalContainers; i++) {
-        if (i === currentContainer) arr.push(Y);
-        else arr.push(NaN);
-    }
-    return arr;
-};
 
 const createChartContainer = (container, el, counter) => {
     const chart_node = document.createElement("div")
@@ -295,35 +228,21 @@ const createGraph = (element, data, labels, title) => {
 });
 };
 
-const displayStatistics = (cpu_load, total_energy, ) => {
-    document.querySelector("#max-cpu-load").innerText = (Math.max.apply(null, cpu_load) / 100) + " %"
-    document.querySelector("#total-energy").innerText = (total_energy / 1000).toFixed(2) + " J"
-    document.querySelector("#total-co2").innerText = (total_energy / 1000 / 3600000 * 0.519 * 1000000).toFixed(2) + " ugCO2eq"
-    document.querySelector("#avg-cpu-load").innerText = ((cpu_load.reduce((a, b) => a + b, 0) / cpu_load.length) / 100).toFixed(2) + " %"
+const displayStatistics = (metrics) => {
+    document.querySelector("#max-cpu-load").innerText = (Math.max.apply(null, metrics.cpu_load) / 100) + " %"
+    document.querySelector("#total-energy").innerText = (metrics.total_energy / 1000).toFixed(2) + " J"
+    document.querySelector("#total-co2").innerText = (metrics.total_energy / 1000 / 3600000 * 0.519 * 1000000).toFixed(2) + " ug"
+    document.querySelector("#avg-cpu-load").innerText = ((metrics.cpu_load.reduce((a, b) => a + b, 0) / metrics.cpu_load.length) / 100).toFixed(2) + " %"
 
-    const total_CO2 = (total_energy / 1000 / 3600000 * 0.519 * 1000000);
-    // const total_CO2_in_kg = total_CO2 / 1000000000; // the real value, bring it back later on
-    const total_CO2_in_kg = total_CO2; // fake value only so that we see numbers greater than 0.00
+    const total_CO2_in_tons = (metrics.total_energy / 1000 / 3600000 * 0.519 * 1000000);
+    const total_CO2_in_kg = total_CO2_in_tons / 1000000000;
 
-    document.querySelector("#trees").innerText = (total_CO2_in_kg / 0.06 / 1000).toFixed(2) + " trees";
-    document.querySelector("#miles-driven").innerText = (total_CO2_in_kg / 0.000403 / 1000).toFixed(2) + " miles driven";
-    document.querySelector("#gasoline").innerText = (total_CO2_in_kg / 0.008887 / 1000).toFixed(2) + " gallons";
-    document.querySelector("#smartphones-charged").innerText = (total_CO2_in_kg / 0.00000822 / 1000).toFixed(2) + " smartphones charged";
-    document.querySelector("#flights").innerText = (total_CO2_in_kg / 1000).toFixed(2) + " flights from Berlin to New York City";
+    document.querySelector("#trees").innerText = (total_CO2_in_kg / 0.06 / 1000).toFixed(2);
+    document.querySelector("#miles-driven").innerText = (total_CO2_in_kg / 0.000403 / 1000).toFixed(2);
+    document.querySelector("#gasoline").innerText = (total_CO2_in_kg / 0.008887 / 1000).toFixed(2);
+    document.querySelector("#smartphones-charged").innerText = (total_CO2_in_kg / 0.00000822 / 1000).toFixed(2);
+    document.querySelector("#flights").innerText = (total_CO2_in_kg / 1000).toFixed(2);
 }
-
-const toggleNotes = () => {
-    const notes = document.getElementsByClassName("dygraph-annotation");
-    console.log("notes", notes);
-    for (let i = 0; i < notes.length; i++) {
-    if (notes[i].style.top !== "") { // prevent the broken notes on top to appear again
-        if (notes[i].style.display === "" || notes[i].style.display === "block")
-            notes[i].style.display = "none";
-        else notes[i].style.display = "block";
-    }
-}
-};
-
 
 
 /* Chart starting code*/
@@ -331,14 +250,12 @@ $(document).ready((e) => {
     const query_string = window.location.search;
     const url_params = (new URLSearchParams(query_string))
 
-    //makeAPICall('/v1/notes/' + url_params.get('id'), (my_json) => getAnnotations(my_json))
+    // makeAPICall('/v1/notes/' + url_params.get('id'), (my_json) => getAnnotations(my_json))
 
     makeAPICall('/v1/stats/single/' + url_params.get('id'), (my_json) => {
-        const my_series = getData(my_json);
+        const metrics = getMetrics(my_json, 'apex');
         fillAvgContainers(my_json.project)
-        displayGraphs(my_series, 'apex');
-        displayStatistics();
+        displayGraphs(metrics.series, 'apex');
+        displayStatistics(metrics);
     })
-
-
 });
