113316413eb131076d4d1d2903eb54c90b26b451
Dan M <dan@green-coding.org>
2022-07-26 11:48:34 +0200

    better string formatting in global_config

--- lib/global_config.py
+++ lib/global_config.py
@@ -9,10 +9,8 @@ class GlobalConfig:
         return cls.instance
 
     def __init__(self, config_name="config.yml"):
-        config_path = "{path}/../"
-        config_path += config_name
         if not hasattr(self, "config"):
-            with open(config_path.format(path=os.path.dirname(os.path.realpath(__file__)))) as config_file:
+            with open("{path}/../{config_name}".format(path=os.path.dirname(os.path.realpath(__file__)),config_name=config_name)) as config_file:
                 self.config = yaml.load(config_file,yaml.FullLoader)
 
 if __name__ == "__main__":
