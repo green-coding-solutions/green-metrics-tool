8f2c3f782c8869bf0b377e94b344369b5e9a2efc
Dan M <dan@green-coding.org>
2022-07-26 16:16:32 +0200

    added ability to pass in pw variable to install/test setup scripts, so they can be used within the CI

--- test/setup-test-env.sh
+++ test/setup-test-env.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
+db_pw=''
+while getopts "p:" o; do
+    case "$o" in
+        p)
+            db_pw=${OPTARG}
+            ;;
+    esac
+done
+
+if [[ -z "$db_pw" ]] ; then
+    read -sp "Please enter the new password to be set for the testing PostgreSQL DB: " db_pw
+fi
+
 echo "Updating config.yml with new password ..."
 cp ../config.yml.example ../test-config.yml
 sed -i -e "s|host: |host: test-|" ../test-config.yml
-sed -i -e "s|PLEASE_CHANGE_THIS|testpw|" ../test-config.yml
+sed -i -e "s|PLEASE_CHANGE_THIS|$db_pw|" ../test-config.yml
 
 echo "Creating test-compose.yml ..."
 cp ../docker/compose.yml.example ../docker/test-compose.yml
 sed -i -e "s|PATH_TO_GREEN_METRICS_TOOL_REPO|$PWD/../|" ../docker/test-compose.yml
-sed -i -e "s|PLEASE_CHANGE_THIS|testpw|" ../docker/test-compose.yml
+sed -i -e "s|PLEASE_CHANGE_THIS|$db_pw|" ../docker/test-compose.yml
 sed -i -e "s|# - TEST_CONFIG_SETUP|- $PWD/../test-config.yml|" ../docker/test-compose.yml
 
 sed -i -e "s|container_name: |container_name: test-|" ../docker/test-compose.yml
