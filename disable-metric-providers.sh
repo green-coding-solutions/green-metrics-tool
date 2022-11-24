83f73598863dc015015f4e269925c626d8ed708e
Dan M <dan@green-coding.org>
2022-11-24 12:22:25 +0100

    - removed todo comments; -disable metric providers script edits config.yml, not test-config.yml

--- disable-metric-providers.sh
+++ disable-metric-providers.sh
@@ -6,13 +6,13 @@ echo "Editing config.yml... "
 
 # First turn on all metric providers
 echo "first turning on all metric providers"
-sed -i "/metric-providers:/,/admin:/s/^#    /    /" test-config.yml
+sed -i "/metric-providers:/,/admin:/s/^#    /    /" config.yml
 
 # Then comment out instances of the word
 for word in "$@"
 do
     echo "disabling metric providers containing $word ..."
-   sed -i "/metric-providers:/,/admin:/s/^.*\.$word\./#&/g" test-config.yml
+   sed -i "/metric-providers:/,/admin:/s/^.*\.$word\./#&/" config.yml
 done
 
 echo "fin"
