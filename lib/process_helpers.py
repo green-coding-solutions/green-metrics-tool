7eee9b77cec93ce86db845f6d164525e07e72ccc
Arne Tarara <arne@datafuse.de>
2023-01-10 16:16:12 +0100

    linting

--- lib/process_helpers.py
+++ lib/process_helpers.py
@@ -42,17 +42,19 @@ def timeout(process, cmd: str, duration: int):
         # This could maybe be optimized with manual code
         # Also if this code is slow on windows it should be reimplemented
         process.wait(duration)
-    except subprocess.TimeoutExpired as exc:
+    except subprocess.TimeoutExpired:
         print(f"Process exceeded runtime of {duration}s. Terminating ...")
         process.terminate()
         try:
             process.wait(5)
-        except subprocess.TimeoutExpired as e:
+        except subprocess.TimeoutExpired:
             print("Process could not terminate in 5s time. Killing ...")
             process.kill()
+            #pylint: disable=raise-missing-from
             raise RuntimeError(f"Process could not terminate in 5s time and was killed: {cmd}")
         # We want to safely kill the process, but still this is considered a critical
         # error condition. Therefore we throw an exception nonetheless to mark it
+        #pylint: disable=raise-missing-from
         raise RuntimeError(f"Process exceeded runtime of {duration}s: {cmd}")
 
 def parse_stream_generator(process, cmd):
