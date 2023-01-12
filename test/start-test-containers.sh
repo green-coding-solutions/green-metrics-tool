c51cfff2df34ec0d386528d20343483132c15565
Arne Tarara <arne@datafuse.de>
2023-01-12 10:53:43 +0100

    Tests minor cleanups (#119)
    * start-test-containers script now takes -d flag to start in detached mode; updated tests readme; workflow now calls start/stop test containers script
    
    * added parameter to test action for gmt root directory
    
    * added test-directory and test-command parameters to gmt-test action
    
    * fixed reference to input variable
    
    * moved commit checking code into its own action, tests-vm workflow file now references this action
    
    * commit checking action now used in bare metal workflow as well
    
    * changed reference to dev
    
    * split schedulaed workflows into seperate ones for dev branch and main branch
    
    * split badge into two badges - for main and dev branch
    
    * removed activity checker action from this branch, it now lives in its own repo; updated references of this action to new repo
    
    Co-authored-by: Dan M <dan@green-coding.org>

--- test/start-test-containers.sh
+++ test/start-test-containers.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 echo "Running docker containers..."
-docker compose -f ../docker/test-compose.yml up --remove-orphans 
+
+detach=false
+
+while getopts "d" opts; do
+    case "$opts" in
+        d) detach=true;;
+    esac
+done
+
+# docker compose with -f flag if force is true
+echo $detach
+
+if [ "$detach" = true ] ; then
+    docker compose -f ../docker/test-compose.yml up -d --remove-orphans 
+else
+    docker compose -f ../docker/test-compose.yml up --remove-orphans
+fi
