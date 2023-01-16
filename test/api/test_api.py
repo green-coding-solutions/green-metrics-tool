a99c1c9479bbba9e4e1661d753bc1c5c459ce73b
Dan M <dan@green-coding.org>
2023-01-16 14:52:52 +0100

    added test in test_runner that checks db for input rows, and checks std.out output against db rows; - moved helper function from test_api to utils; -reorganized test_runner so that it does the e2e run during test setup and saves std.out and std.err to global which is then parsed by the tests

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -10,7 +10,6 @@ sys.path.append(f"{current_dir}/../../lib")
 import utils
 from pydantic import BaseModel
 from global_config import GlobalConfig
-from db import DB
 
 class Project(BaseModel):
     name: str
@@ -27,28 +26,13 @@ PROJECT = Project(name=PROJECT_NAME, url='testURL', email='testEmail')
 def test_post_project_add():
     response = requests.post(f"{API_URL}/v1/project/add", json=PROJECT.dict(), timeout=15)
     assert response.status_code == 200
-    pid = get_pid(PROJECT_NAME)
+    pid = utils.get_pid(PROJECT_NAME)
     assert pid is not None
 
 
 def test_get_projects():
     response = requests.get(f"{API_URL}/v1/projects", timeout=15)
     res_json = response.json()
-    pid = get_pid(PROJECT_NAME)
+    pid = utils.get_pid(PROJECT_NAME)
     assert response.status_code == 200
     assert res_json['data'][0][0] == pid
-
-
-def get_pid(project_name):
-    query = """
-            SELECT
-                id
-            FROM
-                projects
-            WHERE name = %s
-            """
-    data = DB().fetch_one(query, (project_name, ))
-    if (data is None or data == []):
-        return None
-
-    return data[0]
