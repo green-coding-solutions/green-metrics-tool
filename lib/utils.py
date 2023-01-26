89044a31c736f79aa952b980820c981d444e8754
dan-mm <33732895+dan-mm@users.noreply.github.com>
2023-01-26 13:36:20 +0100

    added test in test_runner that checks db for input rows, and checks std.out output against db rows; - moved helper function from test_api to utils; -reorganized test_runner so that it does the e2e run during test setup and saves std.out and std.err to global which is then parsed by the tests (#128)
    Co-authored-by: Dan M <dan@green-coding.org>

--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,28 @@
 import random
 import string
 
+from db import DB
+
 def randomword(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
+
+## Is there a better place for this function to live?
+def get_pid(project_name):
+    query = """
+            SELECT
+                id
+            FROM
+                projects
+            WHERE name = %s
+            """
+    data = DB().fetch_one(query, (project_name, ))
+    if (data is None or data == []):
+        return None
+
+    return data[0]
+
+## Parse a string so that the first letter, and any letter after a _, is capitalized
+## E.g. 'foo_bar' -> 'FooBar'
+def get_pascal_case(in_string):
+    return ''.join([s.capitalize() for s in in_string.split('_')])
