e37e8e684f31690eb9283b0592063a22a2e77dcb
Arne Tarara <arne@datafuse.de>
2023-01-27 10:16:08 +0100

    Refactored index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,13 @@
 (async () => {
+    const dateToYMD = (date) => {
+        var d = date.getDate();
+        var m = date.getMonth() + 1; //Month from 0 to 11
+        var y = date.getFullYear();
+        return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+    }
+
     const compareButton = () => {
         var checkedBoxes = document.querySelectorAll('input[name=chbx-proj]:checked');
-        console.log(checkedBoxes.length);
         var link = "";
         if (checkedBoxes.length == 2) {
             link = "/multi.html?dummy=dummy";
@@ -17,7 +23,6 @@
         checkedBoxes.forEach(checkbox => {
             link += "&pids[]=" + checkbox.value;
         });
-        //console.log(link);
         window.location = link;
     }
 
@@ -31,30 +36,36 @@
     stats_data.data.forEach(el => {
         const li_node = document.createElement("tr");
         const link_node = document.createElement('a');
-        content.push({ title: el[1] });
-        if(el[3] == null) {
-            link_node.innerText =  el[1] + " (no data yet 🔥)";
+        const id = el[0]
+        const name = el[1]
+        let uri = el[2]
+        let branch = el[3]
+        const end_measurement = el[4]
+        const last_run = el[5]
+
+        content.push({ title: name });
+        if(end_measurement == null) {
+            link_node.innerText =  `${name} (no data yet 🔥)`;
         } else {
-            link_node.innerText = el[1];
+            link_node.innerText = name;
         }
-        link_node.title = el[1];
-        link_node.href = "/stats.html?id=" + el[0];
+        link_node.title = name;
+        link_node.href = `/stats.html?id=${id}`;
         li_node.appendChild(link_node);
 
-        uri = el[2]
-        
         // Modify the branch name if the database returned null
-        if (!el[3]) {
-            if (el[2].startsWith("http")) {
-                el[3] = 'main/master'
-                uri = `<a href="${uri}">${uri}</a>`
+        if (uri.startsWith("http")) uri = `<a href="${uri}">${uri}</a>`
+
+        if (!branch) {
+            if (uri.startsWith("http")) {
+                branch = 'main/master'
             }
             else {
-                el[3] = '-'
+                branch = '-'
             }
         }
 
-        li_node.innerHTML = `<td class="td-index">${li_node.innerHTML}</td><td class="td-index">${uri}</td><td class="td-index">${el[3]}</td><td class="td-index">${el[4]}</td><td><input type="checkbox" value="${el[0]}" name="chbx-proj" />&nbsp;</td>`;
+        li_node.innerHTML = `<td class="td-index">${li_node.innerHTML}</td><td class="td-index">${uri}</td><td class="td-index">${branch}</td><td class="td-index"><span title="${last_run}">${dateToYMD(new Date(last_run))}</span></td><td><input type="checkbox" value="${id}" name="chbx-proj" />&nbsp;</td>`;
         document.querySelector("#projects-table").appendChild(li_node);
     });
     $('.ui.search').search({ source: content });
