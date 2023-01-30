4d6ee75ab7ad0939cd6bea9c77b1defb1be8a813
Dan M <dan@green-coding.org>
2023-01-30 18:19:25 +0100

    - moved code from module scope into test function - fixed indentation error - renamed non-constant variables so they dont appear constant - better assert for cleanup_success test

--- test/tools/test_runner.py
+++ test/tools/test_runner.py
@@ -18,50 +18,50 @@ import utils
 from runner import Runner
 from global_config import GlobalConfig
 
-PROJECT_NAME = 'test_' + utils.randomword(12)
-config = GlobalConfig(config_name='test-config.yml').config
-metric_providers_keys = config['measurement']['metric-providers'].keys()
-RUN_STD_ERR = ''
-RUN_STD_OUT = ''
+run_stderr = None
+run_stdout = None
+
+project_name = 'test_' + utils.randomword(12)
 
 
-# Runs once per file before any test
+# Runs once per file before any test(
 # This will run the stress-application e2e through the GMT CLI
 def setup_module(module):
     out = io.StringIO()
     err = io.StringIO()
+
     with redirect_stdout(out), redirect_stderr(err):
         uri = os.path.abspath(os.path.join(
             current_dir, '..', 'stress-application/'))
         subprocess.run(['docker', 'compose', '-f', uri+'/compose.yml', 'build'], check=True)
 
-    pid = DB().fetch_one('INSERT INTO "projects" ("name","uri","email","last_run","created_at") \
-                VALUES \
-                (%s,%s,\'manual\',NULL,NOW()) RETURNING id;', params=(PROJECT_NAME, uri))[0]
+        pid = DB().fetch_one('INSERT INTO "projects" ("name","uri","email","last_run","created_at") \
+                    VALUES \
+                    (%s,%s,\'manual\',NULL,NOW()) RETURNING id;', params=(project_name, uri))[0]
 
-    # Run the application
-    runner = Runner(uri=uri, uri_type='folder', pid=pid)
-    runner.run()
+        # Run the application
+        runner = Runner(uri=uri, uri_type='folder', pid=pid)
+        runner.run()
 
-    global RUN_STD_ERR, RUN_STD_OUT
-    RUN_STD_ERR = err.getvalue()
-    RUN_STD_OUT = out.getvalue()
+    global run_stderr, run_stdout
+    run_stderr = err.getvalue()
+    run_stdout = out.getvalue()
 
 def test_no_errors():
     # Assert that there is no std.err output
-    assert RUN_STD_ERR == ''
+    assert run_stderr == ''
 
 def test_cleanup_success():
     # Assert that Cleanup has run
     assert re.search(
-        '>>>> MEASUREMENT SUCCESSFULLY COMPLETED <<<<<<', RUN_STD_OUT)
+        'MEASUREMENT SUCCESSFULLY COMPLETED', run_stdout)
 
 def test_db_rows_are_written_and_presented():
     # for every metric provider, check that there were rows written in the DB with info for that provider
     # also check (in the same test, to save on a DB call) that the output to STD.OUT
     # "Imported XXX metrics from {metric_provider}" displays the same count as in the DB
 
-    project_id = utils.get_pid(PROJECT_NAME)
+    project_id = utils.get_pid(project_name)
     assert(project_id is not None or project_id != '')
     query = """
             SELECT
@@ -75,6 +75,8 @@ def test_db_rows_are_written_and_presented():
     data = DB().fetch_all(query, (project_id,))
     assert(data is not None or data != [])
 
+    config = GlobalConfig(config_name='test-config.yml').config
+    metric_providers_keys = config['measurement']['metric-providers'].keys()
     metric_providers = [(m.split('.')[-1]) for m in metric_providers_keys]
 
     for d in data:
@@ -87,7 +89,7 @@ def test_db_rows_are_written_and_presented():
         assert d_count > 0
 
         ## Assert the information printed to std.out matches what's in the db
-        match = re.search(rf"Imported \S* (\d+) \S* metrics from\s*{d_provider}", RUN_STD_OUT)
+        match = re.search(rf"Imported \S* (\d+) \S* metrics from\s*{d_provider}", run_stdout)
         assert match is not None
         assert int(match.group(1)) == d_count
 
