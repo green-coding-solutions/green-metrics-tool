a382474bae58c6c4674b4c3c6bce715bcc5c49b6
Arne Tarara <arne@datafuse.de>
2023-02-02 11:16:43 +0100

    install_mac sed no temp file; newline for /etc/hosts

--- install_mac.sh
+++ install_mac.sh
@@ -16,12 +16,12 @@ fi
 
 echo "Updating compose.yml with current path ..."
 cp docker/compose.yml.example docker/compose.yml
-sed -i -e "s|PATH_TO_GREEN_METRICS_TOOL_REPO|$PWD|" docker/compose.yml
-sed -i -e "s|PLEASE_CHANGE_THIS|$db_pw|" docker/compose.yml
+sed -i '' -e "s|PATH_TO_GREEN_METRICS_TOOL_REPO|$PWD|" docker/compose.yml
+sed -i '' -e "s|PLEASE_CHANGE_THIS|$db_pw|" docker/compose.yml
 
 echo "Updating config.yml with new password ..."
 cp config.yml.example config.yml
-sed -i -e "s|PLEASE_CHANGE_THIS|$db_pw|" config.yml
+sed -i '' -e "s|PLEASE_CHANGE_THIS|$db_pw|" config.yml
 
 echo "Adding hardware_info_root.py to sudoers file"
 echo "ALL ALL=(ALL) NOPASSWD:/usr/bin/powermetrics" | sudo tee /etc/sudoers.d/green_coding_powermetrics
@@ -33,13 +33,13 @@ etc_hosts_line_2="127.0.0.1 api.green-coding.local metrics.green-coding.local"
 
 echo "Writing to /etc/hosts file..."
 if ! sudo grep -Fxq "$etc_hosts_line_1" /etc/hosts; then
-    echo "$etc_hosts_line_1" | sudo tee -a /etc/hosts
+    echo -e "\n$etc_hosts_line_1" | sudo tee -a /etc/hosts
 else
     echo "Entry was already present..."
 fi
 
 if ! sudo grep -Fxq "$etc_hosts_line_2" /etc/hosts; then
-    echo "$etc_hosts_line_2" | sudo tee -a /etc/hosts
+    echo -e "\n$etc_hosts_line_2" | sudo tee -a /etc/hosts
 else
     echo "Entry was already present..."
 fi
