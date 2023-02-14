fa0c4a03507f64b47ef626bba6667f7230cec696
Arne Tarara <arne@datafuse.de>
2023-02-14 12:52:23 +0100

    Additions for install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+function print_message {
+    echo ""
+    echo "$1"
+}
+
 db_pw=''
 while getopts "p:" o; do
     case "$o" in
@@ -10,9 +15,16 @@ while getopts "p:" o; do
     esac
 done
 
+read -p "Please enter the desired API endpoint URL: (default: http://api.green-coding.local:9142): " api_url
+api_url=${api_url:-"http://api.green-coding.local:9142"}
+
+read -p "Please enter the desired metrics dashboard URL: (default: http://metrics.green-coding.local:9142): " metrics_url
+metrics_url=${metrics_url:-"http://metrics.green-coding.local:9142"}
+
 if [[ -z "$db_pw" ]] ; then
     read -sp "Please enter the new password to be set for the PostgreSQL DB: " db_pw
 fi
+
 
 echo "Updating compose.yml with current path ..."
 cp docker/compose.yml.example docker/compose.yml
@@ -22,6 +34,21 @@ sed -i '' -e "s|PLEASE_CHANGE_THIS|$db_pw|" docker/compose.yml
 echo "Updating config.yml with new password ..."
 cp config.yml.example config.yml
 sed -i '' -e "s|PLEASE_CHANGE_THIS|$db_pw|" config.yml
+
+print_message "Updating project with provided URLs ..."
+sed -i '' -e "s|__API_URL__|$api_url|" config.yml
+sed -i '' -e "s|__METRICS_URL__|$metrics_url|" config.yml
+cp docker/nginx/api.conf.example docker/nginx/api.conf
+host_api_url=`echo $api_url | sed -E 's/^\s*.*:\/\///g'`
+host_api_url=${host_api_url%:*}
+sed -i '' -e "s|__API_URL__|$host_api_url|" docker/nginx/api.conf
+cp docker/nginx/frontend.conf.example docker/nginx/frontend.conf
+host_metrics_url=`echo $metrics_url | sed -E 's/^\s*.*:\/\///g'`
+host_metrics_url=${host_metrics_url%:*}
+sed -i '' -e "s|__METRICS_URL__|$host_metrics_url|" docker/nginx/frontend.conf
+cp frontend/js/config.js.example frontend/js/config.js
+sed -i '' -e "s|__API_URL__|$api_url|" frontend/js/config.js
+sed -i '' -e "s|__METRICS_URL__|$metrics_url|" frontend/js/config.js
 
 echo "Adding hardware_info_root.py to sudoers file"
 echo "ALL ALL=(ALL) NOPASSWD:/usr/bin/powermetrics" | sudo tee /etc/sudoers.d/green_coding_powermetrics
