3c007a91fc9087a2ab8a7e12399352f7c0db93dc
Danilo Ješić <mutant.krofna@gmail.com>
2023-02-28 16:01:40 +0100

    WIP

--- frontend/js/ci.js
+++ frontend/js/ci.js
@@ -24,6 +24,104 @@ const convertValue = (value, unit) => {
 
 }
 
+const createChartContainer = (container, el) => {
+    const chart_node = document.createElement("div")
+    chart_node.classList.add("card");
+    chart_node.classList.add('statistics-chart-card')
+    chart_node.classList.add('ui')
+
+    chart_node.innerHTML = `
+    <div class="content">
+        <div class="description">
+            <div class="statistics-chart" id=${el}-chart></div>
+        </div>
+    </div>`
+    document.querySelector(container).appendChild(chart_node)
+
+
+    return chart_node.querySelector('.statistics-chart');
+}
+
+const getEChartsOptions = () => {
+    return {
+        tooltip: {
+            trigger: 'axis'
+        },
+        xAxis: {
+            type: 'time',
+            splitLine: {show: true}
+        },
+
+        yAxis: {
+            type: 'value',
+            splitLine: {show: true}
+        },
+        series: [],
+        title: {text: null},
+        animation: false,
+        legend: {
+            data: [],
+            bottom: 0,
+            // type: 'scroll' // maybe active this if legends gets too long
+        },
+        toolbox: {
+            itemSize: 25,
+            top: 55,
+            feature: {
+                dataZoom: {
+                yAxisIndex: 'none'
+                },
+                restore: {}
+            }
+            },
+
+    };
+}
+
+const displayGraph = (runs) => {
+    const element = createChartContainer("#chart-container", "run-energy");
+    var options = getEChartsOptions();
+    options.title.text = `Workflow energy cost over time`;
+
+    console.log(runs)
+    //console.log(run)
+    //options.legend.data.push(run[2])
+    options.series.push({
+            name: 'ci runs',
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            areaStyle: {},
+            data: runs.forEach(),
+            markLine: { data: [ {type: "average",label: {formatter: "AVG_ii:\n{c}"}}]}
+    });
+
+  /*   runs.forEach((run) => {
+        console.log(run)
+        options.legend.data.push(run[2])
+        options.series.push({
+                name: run[2],
+                type: 'line',
+                smooth: true,
+                symbol: 'none',
+                areaStyle: {},
+                data: runs[0].series,
+                markLine: { data: [ {type: "average",label: {formatter: "AVG_ii:\n{c}"}}]}
+        });
+
+    }); */
+
+
+    const chart_instance = echarts.init(element);
+    chart_instance.setOption(options);
+
+    window.onresize = function() { // set callback when ever the user changes the viewport
+        chart_instance.resize();
+    }
+}
+
+
+
 $(document).ready( (e) => {
     (async () => {
         const query_string = window.location.search;
@@ -85,6 +183,7 @@ $(document).ready( (e) => {
             document.querySelector("#badges-table").appendChild(li_node);
         });
        $('table').tablesort();
-
+       displayGraph(badges_data.data)
+       
     })();
 });
