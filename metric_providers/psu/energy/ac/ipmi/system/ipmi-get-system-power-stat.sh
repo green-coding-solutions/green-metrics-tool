05adf81b1f01f2c57c83d62cb78a0c454578a9d3
Danilo Ješić <34022788+djesic@users.noreply.github.com>
2023-03-03 16:41:14 +0100

    Refactoring ipmi reporter (#194)
    * Added command line argument parsing
    
    * Removed && in bash call

--- metric_providers/psu/energy/ac/ipmi/system/ipmi-get-system-power-stat.sh
+++ metric_providers/psu/energy/ac/ipmi/system/ipmi-get-system-power-stat.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
+
+i=''
+
+while getopts "i:" o; do
+    case "$o" in
+        i)
+            i=${OPTARG}
+            ;;
+    esac
+done
+
 while true; do
-    echo -n $(($(date +%s%N)/1000)) "" && sudo /usr/sbin/ipmi-dcmi --get-system-power-statistics | head -1 |  awk '{print $4}'
-    sleep ${2:-0.1}
+    echo -en $(($(date +%s%N)/1000)) $(sudo /usr/sbin/ipmi-dcmi --get-system-power-statistics | head -1 |  awk '{print $4}') "\n"
+    sleep ${i:-0.1}
 
 done
