f2c59ee3a5ffdfb722c93c48cedcb5accebba32d
Arne Tarara <arne@datafuse.de>
2023-03-03 16:49:44 +0100

    Removed extra space

--- metric_providers/psu/energy/ac/ipmi/system/ipmi-get-system-power-stat.sh
+++ metric_providers/psu/energy/ac/ipmi/system/ipmi-get-system-power-stat.sh
@@ -11,7 +11,7 @@ while getopts "i:" o; do
 done
 
 while true; do
-    echo -en $(($(date +%s%N)/1000)) $(sudo /usr/sbin/ipmi-dcmi --get-system-power-statistics | head -1 |  awk '{print $4}') "\n"
+    echo -en $(($(date +%s%N)/1000)) $(sudo /usr/sbin/ipmi-dcmi --get-system-power-statistics | head -1 |  awk '{print $4}')"\n"
     sleep ${i:-0.1}
 
 done
