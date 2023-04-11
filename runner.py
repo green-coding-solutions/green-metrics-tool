c42a925fe8a82be441dc260ae82acc507f3dfb14
Arne Tarara <arne@datafuse.de>
2023-04-11 13:18:54 +0200

    Clearer error message

--- runner.py
+++ runner.py
@@ -569,7 +569,7 @@ class Runner:
                     elif isinstance(service['environment'], dict):
                         env_key, env_value = str(docker_env_var), str(service['environment'][docker_env_var])
                     else:
-                        raise RuntimeError(f"Environment variable needs to be a string with = or dict!")
+                        raise RuntimeError(f"Environment variable needs to be a string with = or dict and non-empty. We do not allow the feature of forwarding variables from the host OS!")
 
                     True, True
                     if not self._allow_unsafe and re.search(r'^[A-Z_]+$', env_key) is None:
