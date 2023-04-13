c0c1b1fa8184df60dea60e0584782aa14b10c506
Arne Tarara <arne@datafuse.de>
2023-04-13 13:11:07 +0200

    Index.js now clustered by projects

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -9,13 +9,19 @@ const compareButton = () => {
 }
 (async () => {
     const dateToYMD = (date) => {
-        let d = date.getDate();
-        let m = date.getMonth() + 1; //Month from 0 to 11
-        let y = date.getFullYear();
-        return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+        let day = date.getDate();
+        day = day <= 9 ? '0' + day : day;
+
+        let month = date.getMonth() + 1; //Month from 0 to 11
+        month = month<=9 ? '0' + month : month;
+        let offset = new Date().getTimezoneOffset();
+        offset = offset < 0 ? `+${-offset/60}` : -offset/60;
+
+
+        return ` ${date.getFullYear()}-${month}-${day} ${date.getHours()}:${date.getMinutes()} UTC${offset}`;
     }
 
-    let content = [];
+    let search = [];
     try {
         var api_data = await makeAPICall('/v1/projects')
     } catch (err) {
@@ -23,56 +29,82 @@ const compareButton = () => {
             return;
     }
     api_data.data.forEach(el => {
-        const li_node = document.createElement("tr");
-        const link_node = document.createElement('a');
+
         const id = el[0]
-        const name = el[1]
+        let name = el[1]
         let uri = el[2]
         let branch = el[3]
         const end_measurement = el[4]
         const last_run = el[5]
         const invalid_project = el[6]
 
-        content.push({ title: name });
-        if(end_measurement == null) {
-            link_node.innerText =  `${name} (no data yet 🔥)`;
-        } else {
-            link_node.innerText = name;
-        }
-        if(invalid_project != null) {
-            link_node.innerHTML = `${name} <span class="ui yellow horizontal label" title="${invalid_project}">invalidated</span>`;
+        let uri_link = '';
+        if (uri.startsWith("http")) {
+            uri_link = `<a href="${uri}"><i class="icon exchange alternate"></i></a>`;
         }
 
 
-        link_node.title = name;
-        link_node.href = `/stats.html?id=${id}`;
-        li_node.appendChild(link_node);
-
-
-        if (!branch) {
-            if (uri.startsWith("http")) {
-                branch = 'main/master'
-            }
-            else {
-                branch = '-'
-            }
+        // insert new accordion row if repository not known
+        let td_node = document.querySelector(`td[data-uri='${uri}']`)
+        if (td_node == null || td_node == undefined) {
+            let row = document.querySelector('#projects-table tbody').insertRow()
+            row.innerHTML = `
+                <td data-uri="${uri}">
+                    <div class="ui accordion" style="width: 100%;">
+                      <div class="title">
+                        <i class="dropdown icon"></i> ${uri} ${uri_link}
+                      </div>
+                      <div class="content">
+                      </div>
+                    </div>
+                </td>
+                <td><input type="checkbox"></td>`;
+            let content = document.querySelector(`#projects-table td[data-uri='${uri}'] div.content`);
+            content.innerHTML = `
+                <table class="ui table">
+                    <thead class="full-width">
+                        <tr>
+                            <th>Name</th>
+                            <th>Branch</th>
+                            <th>Last run</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>`;
         }
+
+
+
+        search.push({ title: name });
+
+        if (branch && uri.startsWith("http")) branch = 'main/master'
+        else branch = '-'
 
         // Modify the branch name if the database returned null
-        if (uri.startsWith("http")) uri = `<a href="${uri}">${uri}</a>`
+        if (uri.startsWith("http")) {
+            uri = `<a href="${uri}">${uri}</a>`;
+        }
+        if(end_measurement == null) name = `${name} (no data yet 🔥)`;
+        if(invalid_project != null) name = `${name} <span class="ui yellow horizontal label" title="${invalid_project}">invalidated</span>`;
 
 
-        li_node.innerHTML = `
-            <td class="td-index">${li_node.innerHTML}</td>
-            <td class="td-index">${uri}</td>
+
+        let inner_row = document.querySelector(`#projects-table td[data-uri='${uri}'] div.content table tbody`).insertRow();
+
+        inner_row.innerHTML = `
+            <td class="td-index"><a href="/stats.html?id=${id}">${name}</a></td>
             <td class="td-index">${branch}</td>
             <td class="td-index"><span title="${last_run}">${dateToYMD(new Date(last_run))}</span></td>
-            <td><input type="checkbox" value="${id}" name="chbx-proj" />&nbsp;</td>
-        `;
-        document.querySelector("#projects-table").appendChild(li_node);
+            <td><input type="checkbox" value="${id}" name="chbx-proj"/>&nbsp;</td>`;
+
+
     });
-    $('.ui.search').search({ source: content });
-    $('table').tablesort();
+
+    $('.ui.search').search({ source: search });
+    $('.ui.accordion')
+      .accordion()
+    ;
 
 
 })();
