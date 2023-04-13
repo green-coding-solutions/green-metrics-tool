0e2e93a717292ef66b9b930fde03328ef905254b
Danilo Ješić <mutant.krofna@gmail.com>
2023-04-13 13:30:19 +0200

    Implement conversion to Joules

--- metric_providers/psu/energy/ac/ipmi/system/provider.py
+++ metric_providers/psu/energy/ac/ipmi/system/provider.py
@@ -9,7 +9,24 @@ class PsuEnergyAcIpmiSystemProvider(BaseMetricProvider):
             metric_name="psu_energy_ac_ipmi_system",
             metrics={"time": int, "value": int},
             resolution=0.001 * resolution,
-            unit="W",
+            unit="J",
             current_dir=os.path.dirname(os.path.abspath(__file__)),
             metric_provider_executable="ipmi-get-system-power-stat.sh",
         )
+
+    def read_metrics(self, project_id, containers):
+        df = super().read_metrics(project_id, containers)
+
+        # the bash script returns data in Watts, but we need it in Joules
+        # retaining the functionality in case we need it Watts at some point
+        if self._unit == 'W':
+            return df
+
+        # Conversion to Joules
+        intervals = df["time"].diff()
+        intervals[0] = intervals.mean()  # approximate first interval
+        df["interval"] = intervals  # in nanoseconds
+        df["value"] = df.apply(lambda x: x["value"] * x["interval"] / 1_000_000_000, axis=1)
+        df = df.drop(columns="interval")  # clean up
+
+        return df
