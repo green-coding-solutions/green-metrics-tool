6ea5a4801c627543b2ee5fe990853cb0f8ea1ba1
Pottumuttu, Sai Teja <sai.teja.pottumuttu@intel.com>
2023-05-02 14:42:50 +0530

    Adds option to change units to metric
    Co-authored-by: Chauhan, Shekhar <shekharbarca10@gmail.com>
    Signed-off-by: Pottumuttu, Sai Teja <saiteja13427@gmail.com>

--- frontend/js/dashboard_settings.js
+++ frontend/js/dashboard_settings.js
@@ -7,6 +7,9 @@ var remove_idle = localStorage.getItem('remove_idle');
 if(remove_idle == 'true') remove_idle = true;
 else remove_idle = false;
 
+var display_in_metric_units = false;
+if(display_in_metric_units == 'true') display_in_metric_units = true;
+else display_in_metric_units = false;
 
 let toggleWatts = () => {
     localStorage.setItem('display_in_watts', !display_in_watts);
@@ -18,6 +21,10 @@ let toggleRemoveIdle = () => {
     window.location.reload();
 }
 
+let toggleUnits = () => {
+    localStorage.setItem('display_in_metric_units', !display_in_metric_units);
+    window.location.reload();
+}
 
 
 (() => {
@@ -27,8 +34,10 @@ let toggleRemoveIdle = () => {
 
       if(display_in_watts) $("#energy-display").text("Currently showing Watts");
       else $("#energy-display").text("Currently showing Joules");
-     if(remove_idle) $("#remove-idle").text("Currently hiding idle");
+      if(remove_idle) $("#remove-idle").text("Currently hiding idle");
       else $("#remove-idle").text("Currently showing idle");
+      if(display_in_metric_units) $("#units-display").text("Currently showing metric units");
+      else $("#units-display").text("Currently showing imperial units");
 
     });
 
