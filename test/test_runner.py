0352c728fb3c2411f88ae87ad7554fd5679e21f3
Danilo Ješić <mutant.krofna@gmail.com>
2023-05-02 11:06:20 +0200

    Updated tests

--- test/test_runner.py
+++ test/test_runner.py
@@ -1,31 +1,34 @@
 import os
 import sys
+from shutil import copy2
 
 import pytest
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_DATA_CONFIG_DIR = os.path.join(CURRENT_DIR, "data", "config_files")
+REPO_ROOT = os.path.realpath(os.path.join(CURRENT_DIR, ".."))
 sys.path.append(f"{CURRENT_DIR}/../.")
 sys.path.append(f"{CURRENT_DIR}/../lib/")
 
 #pylint:disable=import-error
 from runner import Runner
-# from global_config import GlobalConfig
+from global_config import GlobalConfig
 
 
 test_data = [
-    (True, True),
-    (False, True),
+    ("two_psu_providers.yml", True, True),
+    ("two_psu_providers.yml", False, False),
 ]
 
-@pytest.mark.parametrize("skip_config_check,expected_return", test_data)
-def test_check_configuration(skip_config_check, expected_return):
+@pytest.mark.parametrize("config_file,skip_config_check,expected_return", test_data)
+def test_check_configuration(config_file, skip_config_check, expected_return):
 
     runner = Runner("foo", "baz", "bar", skip_config_check=skip_config_check)
-
-    # TODO: override config for tests? Found no test configs in test data
-    # GlobalConfig(config_name="some.yml")
-    # but it will look at repo root for it - why only repo root?
+    copy2(os.path.join(TEST_DATA_CONFIG_DIR, config_file), os.path.join(REPO_ROOT, config_file))
+    GlobalConfig(config_name=config_file)
 
     ret = runner.check_configuration()
+
+    os.remove(os.path.join(REPO_ROOT, config_file))
 
     assert ret == expected_return
