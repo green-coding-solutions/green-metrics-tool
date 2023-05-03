af4ccb15b51900f7e7f1ef92377e84e83e118595
Sai Teja <saiteja13427@gmail.com>
2023-05-03 17:23:33 +0530

    Extend the error helper to log to file
    Co-authored-by: Chauhan, Shekhar <shekharbarca10@gmail.com>
    Signed-off-by: Pottumuttu, Sai Teja <saiteja13427@gmail.com>

--- lib/error_helpers.py
+++ lib/error_helpers.py
@@ -1,6 +1,7 @@
 import sys
 import traceback
 from terminal_colors import TerminalColors
+from global_config import GlobalConfig
 
 
 def end_error(*errors):
@@ -26,6 +27,16 @@ def format_error(*errors):
 
 
 def log_error(*errors):
+    error_file = GlobalConfig().config['error']['file']
+
+    if error_file != 'None':
+        with open(error_file, 'w') as file:
+            print('\n\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 0_o >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n', file=file)
+            print('Error: ', *errors, file=file)
+            print('\n\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 0_o >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n', file=file)
+            print(traceback.format_exc(), file=file)
+            print('\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 0_o >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n', file=file)
+
     print(TerminalColors.FAIL,
           '\n\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 0_o >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n', file=sys.stderr)
     print('Error: ', *errors, file=sys.stderr)
