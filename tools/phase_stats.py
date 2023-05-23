48a39bee810b5b07754cc274e7ec9b34e69335df
Didi Hoffmann <39629+ribalba@users.noreply.github.com>
2023-05-23 15:01:52 +0200

    Update phase_stats.py
    More output for testing

--- tools/phase_stats.py
+++ tools/phase_stats.py
@@ -15,6 +15,7 @@ from db import DB
 
 
 def generate_csv_line(project_id, metric, detail_name, phase_name, value, value_type, max_value, unit):
+    print(f"--> {project_id},{metric},{detail_name},{phase_name},{round(value)},{value_type},{max_value or ''},{unit},NOW()\n")
     return f"{project_id},{metric},{detail_name},{phase_name},{round(value)},{value_type},{max_value or ''},{unit},NOW()\n"
 
 def build_and_store_phase_stats(project_id):
