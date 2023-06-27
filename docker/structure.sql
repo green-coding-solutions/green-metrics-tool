3af2e828d35d834c0bb87c4e7b15493ea19c839c
Didi Hoffmann <39629+ribalba@users.noreply.github.com>
2023-06-27 09:11:27 +0200

    Adds support for a distributed cluster setup. (#210)
    * Adds support for a distributed cluster setup.
    
    * update to new machine config
    
    * Delete user-data
    
    * Delete gmt_installer.sh
    
    * Delete nop_configure.sh
    
    * Delete meta-data
    
    ---------
    
    Co-authored-by: Arne Tarara <arne@datafuse.de>

--- docker/structure.sql
+++ docker/structure.sql
@@ -105,3 +105,12 @@ CREATE TABLE ci_measurements (
     project_id uuid REFERENCES projects(id) ON DELETE SET NULL ON UPDATE CASCADE DEFAULT null,
     created_at timestamp with time zone DEFAULT now()
 );
+
+CREATE TABLE client_status (
+    id SERIAL PRIMARY KEY,
+	status_code TEXT NOT NULL,
+	machine_id int REFERENCES machines(id) ON DELETE SET NULL ON UPDATE CASCADE DEFAULT null,
+	"data" TEXT,
+	project_id uuid REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    created_at timestamp with time zone DEFAULT now()
+);
