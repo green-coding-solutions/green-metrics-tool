08ca36f640a02d6e6f49d96bd881fdf0a3ae9ac2
dan-mm <dan@green-coding.berlin>
2023-07-23 17:27:41 +0200

    added workflow for building and pushing containers from docker/auxiliary-containers subfolders

---
+++ docker/auxiliary-containers/build-containers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Get the list of subdirectories within "auxiliary-containers" directory containing a Dockerfile
+subdirs=($(find ./docker/auxiliary-containers -type f -name 'Dockerfile' -exec dirname {} \;))
+
+# Loop through each subdirectory, build and push the Docker image
+for subdir in "${subdirs[@]}"; do
+  folder=$(basename "${subdir}")
+  docker buildx build \
+    --push \
+    --tag "ghcr.io/${GITHUB_REPOSITORY}/${folder}:latest" \
+    --platform linux/amd64,linux/arm64 \
+    "${subdir}"
+done
