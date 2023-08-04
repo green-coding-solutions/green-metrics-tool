22e4366eb32137adf2ccd0ed6366456788017b98
Arne Tarara <arne@datafuse.de>
2023-08-04 15:45:16 +0200

    Bugfix: Phases ordering was wrong way round

--- frontend/js/helpers/phase-stats.js
+++ frontend/js/helpers/phase-stats.js
@@ -75,13 +75,13 @@ const createPhaseTab = (phase) => {
         cloned_tab_node.style.display = '';
         cloned_tab_node.innerText = phase;
         cloned_tab_node.setAttribute('data-tab', phase);
-        runtime_tab_node.parentNode.insertBefore(cloned_tab_node, runtime_tab_node)
+        runtime_tab_node.parentNode.appendChild(cloned_tab_node)
 
         let phase_step_node = document.querySelector('.runtime-tab');
         let cloned_step_node = phase_step_node.cloneNode(true);
         cloned_step_node.style.display = '';
         cloned_step_node.setAttribute('data-tab', phase);
-        phase_step_node.parentNode.insertBefore(cloned_step_node, phase_step_node)
+        phase_step_node.parentNode.appendChild(cloned_step_node)
     }
 }
 
