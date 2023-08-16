698e42f073d2a9b7f9c75a32b1093c42e8fe7cbb
Arne Tarara <arne@datafuse.de>
2023-08-16 10:29:52 +0200

    Timeline comparison (#420)
    * Multi Commit View deactivated
    
    * Added timeline base functionality
    
    * Added compare feature
    
    * Added update tool
    
    * commit_timestamp must be time stamp with time zone
    
    * Bugfix: When Repeated Run is selected, but only one item is found (missing phase_stats) still show correct table entries
    
    * Merge
    
    * WIP
    
    * Bugfix missed to add in commit
    
    * Config variables now in config.js.example
    
    * Added commit color info
    
    * HTML validity fixes
    
    * Badge click fix
    
    * Test fix for sanitze
    
    * Sane is now short and default for YMD display
    
    * Added sorting, value formatting, date showing and display of the URI, filename and branch on the landing page

--- frontend/js/stats.js
+++ frontend/js/stats.js
@@ -222,7 +222,7 @@ const displayTimelineCharts = (metrics, notes) => {
 
     for ( metric_name in metrics) {
 
-        const element = createChartContainer("#chart-container", metric_name);
+        const element = createChartContainer("#chart-container", `${METRIC_MAPPINGS[metric_name]['clean_name']} via ${METRIC_MAPPINGS[metric_name]['source']} <i data-tooltip="${METRIC_MAPPINGS[metric_name]['explanation']}" data-position="bottom center" data-inverted><i class="question circle icon link"></i></i>`);
 
         let legend = [];
         let series = [];
@@ -262,7 +262,7 @@ const displayTimelineCharts = (metrics, notes) => {
         });
 
         const chart_instance = echarts.init(element);
-        let options = getLineBarChartOptions(legend, series, 'Time', metrics[metric_name].converted_unit);
+        let options = getLineBarChartOptions(null, legend, series, 'Time', metrics[metric_name].converted_unit);
         chart_instance.setOption(options);
         chart_instances.push(chart_instance);
 
