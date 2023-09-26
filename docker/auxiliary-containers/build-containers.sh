5b8dbf6cf637822a5cfdd26c74c8c61f3b4cec63
dan-mm <dan@green-coding.berlin>
2023-09-26 15:34:24 +0200

    tag our auxiliary containers with an incrementing version number, instead of latest

--- docker/auxiliary-containers/build-containers.sh
+++ docker/auxiliary-containers/build-containers.sh
@@ -2,6 +2,17 @@
 
 set -euo pipefail
 
+# the first arguement should be the version number
+
+version=$1
+
+# check that version is a non-empty string representing a number
+if ! [[ ${version} =~ ^[0-9]+$ ]]; then
+  echo "Usage: $0 <version-number>"
+  echo "Example: $0 1"
+  exit 1
+fi
+
 # The names of the folders within "auxiliary-containers" must match the repository name in dockerhub!
 
 # Get the list of subdirectories within "auxiliary-containers" directory containing a Dockerfile
@@ -12,7 +23,7 @@ for subdir in "${subdirs[@]}"; do
   folder=$(basename "${subdir}")
   docker buildx build \
     --push \
-    --tag "greencoding/${folder}:latest" \
+    --tag "greencoding/${folder}:v1.${version}" \
     --platform linux/amd64,linux/arm64 \
     "${subdir}"
 done
