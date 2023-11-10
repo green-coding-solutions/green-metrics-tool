3e3644f63724c504c3154d964a84c54b9c51ec08
dan-mm <dan@green-coding.berlin>
2023-11-10 18:15:46 +0100

    print_message -> echo

--- .devcontainer/on-start.sh
+++ .devcontainer/on-start.sh
@@ -12,7 +12,7 @@ host_api_url=${host_api_url%:*}
 etc_hosts_line_1="127.0.0.1 green-coding-postgres-container"
 etc_hosts_line_2="127.0.0.1 ${host_api_url} ${host_metrics_url}"
 
-print_message "Writing to /etc/hosts file..."
+echo "Writing to /etc/hosts file..."
 
 # Entry 1 is needed for the local resolution of the containers through the jobs.py and runner.py
 if ! sudo grep -Fxq "$etc_hosts_line_1" /etc/hosts; then
