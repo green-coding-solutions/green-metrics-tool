6e45143f74c6df34a3a421d25f39c0af99af7a0d
dan-mm <33732895+dan-mm@users.noreply.github.com>
2023-11-16 16:25:37 +0100

    fix spacing in port setup

--- .devcontainer/codespace-setup.sh
+++ .devcontainer/codespace-setup.sh
@@ -13,4 +13,4 @@ fi
 /workspaces/green-metrics-tool/install_linux.sh -p testpw -a "https://${CODESPACE_NAME}-9142.app.github.dev" -m "https://${CODESPACE_NAME}-9142.app.github.dev" -tsed -i 's/listen \[::\]:9142;/listen [::]:9143;/; s/listen 9142;/listen 9143;/' /somewhere/file.conf
 
 sed -i 's/listen \[::\]:9142;/listen [::]:9143;/; s/listen 9142;/listen 9143;/' /workspaces/green-metrics-tool/docker/nginx/frontend.conf
-sed -i '/green-coding-nginx:/,/green-coding-gunicorn:/ s/\(- 9142:80\)/- 9142:9142\n    - 9143:9143/' /workspaces/green-metrics-tool/docker/compose.yml
+sed -i '/green-coding-nginx:/,/green-coding-gunicorn:/ s/\(- 9142:80\)/- 9142:9142\n      - 9143:9143/' /workspaces/green-metrics-tool/docker/compose.yml
