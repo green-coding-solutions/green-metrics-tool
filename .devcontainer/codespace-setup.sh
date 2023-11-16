20fba98a09077b4f796bff0cf6a0f8ae9e68e316
dan-mm <33732895+dan-mm@users.noreply.github.com>
2023-11-16 17:01:38 +0100

    fix port name for metrics, removed accidental paste

--- .devcontainer/codespace-setup.sh
+++ .devcontainer/codespace-setup.sh
@@ -10,7 +10,7 @@ if [ -f /workspaces/green-metrics-tool/metric_providers/lm_sensors/Makefile ]; t
     mv /workspaces/green-metrics-tool/metric_providers/lm_sensors/Makefile /workspaces/green-metrics-tool/metric_providers/lm_sensors/Makefile.bak
 fi
 
-/workspaces/green-metrics-tool/install_linux.sh -p testpw -a "https://${CODESPACE_NAME}-9142.app.github.dev" -m "https://${CODESPACE_NAME}-9142.app.github.dev" -tsed -i 's/listen \[::\]:9142;/listen [::]:9143;/; s/listen 9142;/listen 9143;/' /somewhere/file.conf
+/workspaces/green-metrics-tool/install_linux.sh -p testpw -a "https://${CODESPACE_NAME}-9142.app.github.dev" -m "https://${CODESPACE_NAME}-9143.app.github.dev"
 
 sed -i 's/listen \[::\]:9142;/listen [::]:9143;/; s/listen 9142;/listen 9143;/' /workspaces/green-metrics-tool/docker/nginx/frontend.conf
 sed -i '/green-coding-nginx:/,/green-coding-gunicorn:/ s/\(- 9142:80\)/- 9142:9142\n      - 9143:9143/' /workspaces/green-metrics-tool/docker/compose.yml
