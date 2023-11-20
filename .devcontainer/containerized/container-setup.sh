02aa54935f2f6c6b686e51ac9a0e31a927f7fa3b
dan-mm <dan@green-coding.berlin>
2023-11-20 11:58:55 +0100

    turn off all but one psu providers

--- .devcontainer/containerized/container-setup.sh
+++ .devcontainer/containerized/container-setup.sh
@@ -9,4 +9,4 @@ python3 -m pip install -r /workspaces/green-metrics-tool/metric_providers/psu/en
 sed -i 's/listen \[::\]:9142;/listen [::]:9143;/; s/listen 9142;/listen 9143;/' /workspaces/green-metrics-tool/docker/nginx/frontend.conf
 sed -i '/green-coding-nginx:/,/green-coding-gunicorn:/ s/\(- 9142:80\)/- 9142:9142\n      - 9143:9143/' /workspaces/green-metrics-tool/docker/compose.yml
 
-python3 /workspaces/green-metrics-tool/disable_metric_providers.py --categories RAPL Machine Sensors Debug common --providers NetworkIoCgroupContainerProvider
+python3 /workspaces/green-metrics-tool/disable_metric_providers.py --categories RAPL Machine Sensors Debug --providers NetworkIoCgroupContainerProvider NetworkConnectionsProxyContainerProvider PsuEnergyAcSdiaMachineProvider
