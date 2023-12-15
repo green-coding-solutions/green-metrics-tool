8effee5170710570a12d19804c1bfd1fe6957466
dan-mm <dan@green-coding.berlin>
2023-12-15 14:55:29 +0100

    removed unused functions in rapl providers system checks

--- metric_providers/cpu/energy/RAPL/MSR/component/provider.py
+++ metric_providers/cpu/energy/RAPL/MSR/component/provider.py
@@ -13,19 +13,6 @@ class CpuEnergyRaplMsrComponentProvider(BaseMetricProvider):
             current_dir=os.path.dirname(os.path.abspath(__file__)),
         )
 
-    # TODO: we get a permissions error here. is this expected?
-    def check_system_with_permissions(self):
-        file_path = "/dev/cpu/0/msr"
-        if os.path.exists(file_path):
-            try:
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    file.read()
-            except PermissionError as exc:
-                raise MetricProviderConfigurationError(f"{self._metric_name} provider could not be started.\nCannot read the RAPL MSR at {file_path}.\n\nAre you running in a VM / cloud / shared hosting?\nIf so please disable the {self._metric_name} provider in the config.yml") from exc
-        else:
-            raise MetricProviderConfigurationError(f"{self._metric_name} provider could not be started.\nCould not find the path for the RAPL MSR at {file_path}.\n\nAre you running in a VM / cloud / shared hosting? \nIf so please disable the {self._metric_name} provider in the config.yml")
-
-    # only check for existence
     def check_system(self):
         ps = subprocess.run(['./metric-provider-binary', '-c'], capture_output=True, text=True, check=False)
         if ps.returncode != 0:
