68f3cfd490512e96e7bd2e39e3f6b862df4dc556
Didi Hoffmann <didi@ribalba.de>
2023-12-24 14:42:30 +0100

    pylint fixes and better error handling

--- runner.py
+++ runner.py
@@ -850,15 +850,17 @@ class Runner:
                         status_output = subprocess.check_output(
                             ["docker", "container", "inspect", "-f", "{{.State.Status}}", dependent_container],
                             stderr=subprocess.STDOUT,
-                            text=True
+                            encoding='utf-8'
                         )
+
                         state = status_output.strip()
+
                         if state == "running":
-                            break;
-                        else:
-                            print(f"State of container '{dependent_container}': {state}. Waiting for 1 second")
-                            self.custom_sleep(1)
-                            time_waited += 1
+                            break
+
+                        print(f"State of container '{dependent_container}': {state}. Waiting for 1 second")
+                        self.custom_sleep(1)
+                        time_waited += 1
 
                     if state != "running":
                         raise RuntimeError(f"Dependent container '{dependent_container}' of '{container_name}' is not running after waiting for {time_waited} sec! Consider checking your service configuration, the entrypoint of the container or the logs of the container.")
