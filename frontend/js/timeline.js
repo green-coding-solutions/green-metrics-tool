9389c9409ef21a405752bf61246e16b4d965f186
Arne Tarara <arne@datafuse.de>
2023-12-27 20:36:19 +0100

    GMT color via own commit hash (#634)
    * GMT can now sort via own commit hash
    
    * Clicking now shows proper fixed place menu

--- frontend/js/timeline.js
+++ frontend/js/timeline.js
@@ -39,13 +39,13 @@ function* colorIterator() {
     }
 }
 
-const generateColoredValues = (values) => {
+const generateColoredValues = (values, key) => {
     const color_iterator = colorIterator()
-    let last_commit_hash = null
+    let last_hash = null
     let color = null;
     return values.map((value) => {
-        if(last_commit_hash != value.commit_hash) {
-            last_commit_hash = value.commit_hash
+        if(last_hash != value[key]) {
+            last_hash = value[key]
             color = color_iterator.next().value
         }
         return {value: value.value, itemStyle: {color: color}}
@@ -159,7 +159,7 @@ const loadCharts = async () => {
     let prun_id = null
 
     phase_stats_data.forEach( (data) => {
-        let [run_id, run_name, created_at, metric_name, detail_name, phase, value, unit, commit_hash, commit_timestamp] = data
+        let [run_id, run_name, created_at, metric_name, detail_name, phase, value, unit, commit_hash, commit_timestamp, gmt_hash] = data
 
 
         if (series[`${metric_name} - ${detail_name}`] == undefined) {
@@ -167,7 +167,7 @@ const loadCharts = async () => {
         }
 
         series[`${metric_name} - ${detail_name}`].labels.push(commit_timestamp)
-        series[`${metric_name} - ${detail_name}`].values.push({value: value, commit_hash: commit_hash})
+        series[`${metric_name} - ${detail_name}`].values.push({value: value, commit_hash: commit_hash, gmt_hash: gmt_hash})
         series[`${metric_name} - ${detail_name}`].notes.push({
             run_name: run_name,
             created_at: created_at,
@@ -176,6 +176,7 @@ const loadCharts = async () => {
             phase: phase,
             run_id: run_id,
             prun_id: prun_id,
+            gmt_hash: gmt_hash,
         })
 
         prun_id = run_id
@@ -203,7 +204,7 @@ const loadCharts = async () => {
 
         const chart_instance = echarts.init(element);
 
-        const my_values = generateColoredValues(series[my_series].values);
+        const my_values = generateColoredValues(series[my_series].values, $('.radio-coloring:checked').val());
 
         let data_series = [{
             name: my_series,
@@ -221,27 +222,24 @@ const loadCharts = async () => {
         let options = getLineBarChartOptions([], series[my_series].labels, data_series, 'Time', series[my_series].unit,  'category', null, false, null, true, false, true);
 
         options.tooltip = {
-            trigger: 'item',
+            triggerOn: 'click',
             formatter: function (params, ticket, callback) {
                 if(series[params.seriesName]?.notes == null) return; // no notes for the MovingAverage
                 return `<strong>${series[params.seriesName].notes[params.dataIndex].run_name}</strong><br>
+                        run_id: <a href="/stats.html?id=${series[params.seriesName].notes[params.dataIndex].run_id}"  target="_blank">${series[params.seriesName].notes[params.dataIndex].run_id}</a><br>
                         date: ${series[params.seriesName].notes[params.dataIndex].created_at}<br>
                         metric_name: ${params.seriesName}<br>
                         phase: ${series[params.seriesName].notes[params.dataIndex].phase}<br>
                         value: ${numberFormatter.format(series[params.seriesName].values[params.dataIndex].value)}<br>
                         commit_timestamp: ${series[params.seriesName].notes[params.dataIndex].commit_timestamp}<br>
-                        commit_hash: ${series[params.seriesName].notes[params.dataIndex].commit_hash}<br>
+                        commit_hash: <a href="${$("#uri").text()}/commit/${series[params.seriesName].notes[params.dataIndex].commit_hash}" target="_blank">${series[params.seriesName].notes[params.dataIndex].commit_hash}</a><br>
+                        gmt_hash: <a href="https://github.com/green-coding-berlin/green-metrics-tool/commit/${series[params.seriesName].notes[params.dataIndex].gmt_hash}" target="_blank">${series[params.seriesName].notes[params.dataIndex].gmt_hash}</a><br>
+
                         <br>
-                        <i>Click to diff measurement with previous</i>
+                        👉 <a href="/compare.html?ids=${series[params.seriesName].notes[params.dataIndex].run_id},${series[params.seriesName].notes[params.dataIndex].prun_id}" target="_blank">Diff with previous run</a>
                         `;
             }
         };
-
-        chart_instance.on('click', function (params) {
-            if(params.componentType != 'series') return; // no notes for the MovingAverage
-            window.open(`/compare.html?ids=${series[params.seriesName].notes[params.dataIndex].run_id},${series[params.seriesName].notes[params.dataIndex].prun_id}`, '_blank');
-
-        });
 
         options.dataZoom = {
             show: false,
