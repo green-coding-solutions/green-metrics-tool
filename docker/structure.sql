67bb9e8f3154e34d3df077e37730b951523e69ed
Arne Tarara <arne@datafuse.de>
2024-03-12 12:12:27 +0100

    Removed artifacts table

--- docker/structure.sql
+++ docker/structure.sql
@@ -290,19 +290,3 @@ CREATE TRIGGER hog_tasks_moddatetime
 
 CREATE INDEX idx_task_coalition ON hog_tasks(coalition);
 
-
-CREATE TABLE artifacts (
-    id SERIAL PRIMARY KEY,
-    type text,
-    key text,
-    data json,
-    created_at timestamp with time zone DEFAULT now(),
-    updated_at timestamp with time zone
-);
-
-CREATE TRIGGER artifacts_moddatetime
-    BEFORE UPDATE ON artifacts
-    FOR EACH ROW
-    EXECUTE PROCEDURE moddatetime (updated_at);
-
-CREATE INDEX idx_artifact_select ON artifacts(type, key);
