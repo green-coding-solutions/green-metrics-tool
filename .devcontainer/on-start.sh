981770a64a0b5180d2bc7105537cdcaae0c07a51
Arne Tarara <arne@datafuse.de>
2024-05-04 13:58:36 +0200

    Fixes for domain resolution in codespaces

--- .devcontainer/on-start.sh
+++ .devcontainer/on-start.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-metrics_url="http://metrics.green-coding.internal:9143"
-host_metrics_url=`echo $metrics_url | sed -E 's/^\s*.*:\/\///g'`
-host_metrics_url=${host_metrics_url%:*}
-
-api_url="http://api.green-coding.internal:9142"
-host_api_url=`echo $api_url | sed -E 's/^\s*.*:\/\///g'`
-host_api_url=${host_api_url%:*}
-
 etc_hosts_line_1="127.0.0.1 green-coding-postgres-container"
-etc_hosts_line_2="127.0.0.1 ${host_api_url} ${host_metrics_url}"
 
 echo "Writing to /etc/hosts file..."
 
@@ -19,13 +10,4 @@ if ! sudo grep -Fxq "$etc_hosts_line_1" /etc/hosts; then
     echo "$etc_hosts_line_1" | sudo tee -a /etc/hosts
 else
     echo "Entry was already present..."
-fi
-
-# Entry 2 can be external URLs. These should not resolve to localhost if not explcitely wanted
-if [[ ${host_metrics_url} == *".green-coding.internal"* ]];then
-    if ! sudo grep -Fxq "$etc_hosts_line_2" /etc/hosts; then
-        echo "$etc_hosts_line_2" | sudo tee -a /etc/hosts
-    else
-        echo "Entry was already present..."
-    fi
 fi
--- .devcontainer/splash.md
+++ .devcontainer/splash.md
@@ -2,10 +2,12 @@
 
 Thank you for trying out the Green Metrics Tool :-)
 
-Please run `source .devcontainer/workshop/codespace-setup.sh` to set up everything! 🚀
+Please run `bash .devcontainer/workshop/codespace-setup.sh` to set up everything! 🚀
 
-It will take about 60 seconds.
+It will take about 3 minutes.
 
-Do your first run like this: `python3 runner.py --name test --uri /workspaces/green-metrics-tool/example-applications/stress/`
+Load the env: `source venv/bin/activate`
+
+Do your first run like this: `python3 runner.py --name test --uri /workspaces/green-metrics-tool/example-applications/stress/ --skip-system-checks --dev-no-optimizations --dev-no-build`
 
 To see the Metrics front end, go to your ports tab and follow the forwarding address for port 9143
