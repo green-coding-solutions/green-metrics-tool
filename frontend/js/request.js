bcbe813d1c322200ad8b1ba5e54144e7a490a27d
Didi Hoffmann <didi@ribalba.de>
2024-08-25 20:03:05 +0200

    Escapes the paramters

--- frontend/js/request.js
+++ frontend/js/request.js
@@ -4,25 +4,25 @@
         const urlParams = new URLSearchParams(window.location.search);
 
         if (urlParams.has('name')) {
-            document.querySelector('input[name="name"]').value = urlParams.get('name');
+            document.querySelector('input[name="name"]').value = escapeString(urlParams.get('name'));
         }
         if (urlParams.has('email')) {
-            document.querySelector('input[name="email"]').value = urlParams.get('email');
+            document.querySelector('input[name="email"]').value = escapeString(urlParams.get('email'));
         }
         if (urlParams.has('url')) {
-            document.querySelector('input[name="url"]').value = urlParams.get('url');
+            document.querySelector('input[name="url"]').value = escapeString(urlParams.get('url'));
         }
         if (urlParams.has('filename')) {
-            document.querySelector('input[name="filename"]').value = urlParams.get('filename');
+            document.querySelector('input[name="filename"]').value = escapeString(urlParams.get('filename'));
         }
         if (urlParams.has('branch')) {
-            document.querySelector('input[name="branch"]').value = urlParams.get('branch');
+            document.querySelector('input[name="branch"]').value = escapeString(urlParams.get('branch'));
         }
         if (urlParams.has('machine_id')) {
-            document.querySelector('select[name="machine_id"]').value = urlParams.get('machine_id');
+            document.querySelector('select[name="machine_id"]').value = escapeString(urlParams.get('machine_id'));
         }
         if (urlParams.has('schedule_mode')) {
-            document.querySelector('select[name="schedule_mode"]').value = urlParams.get('schedule_mode');
+            document.querySelector('select[name="schedule_mode"]').value = escapeString(urlParams.get('schedule_mode'));
         }
     }
 
