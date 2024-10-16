dfd9a4e46b3ad7c645a80f8e1ce3b65ed794d6e8
Arne Tarara <arne@datafuse.de>
2024-10-16 11:49:29 +0200

    Refactoring for locator access

--- tests/frontend/test_frontend.py
+++ tests/frontend/test_frontend.py
@@ -47,7 +47,7 @@ def setup_and_cleanup_module():
 def test_home():
 
     page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
-    value = page.wait_for_selector("#runs-table > tbody > tr:nth-child(2) > td:nth-child(1) > a").text_content()
+    value = page.locator("#runs-table > tbody > tr:nth-child(2) > td:nth-child(1) > a").text_content()
 
     assert value== 'Stress Test #4'
 
@@ -55,45 +55,45 @@ def test_ci():
     page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
     page.get_by_role("link", name="Eco-CI").click()
 
-    page.wait_for_selector("#repositories-table > tbody > tr:nth-child(1) > td > div > div.title").click()
-    page.wait_for_selector('#DataTables_Table_0 > tbody > tr > td.sorting_1 > a').click()
+    page.locator("#repositories-table > tbody > tr:nth-child(1) > td > div > div.title").click()
+    page.locator('#DataTables_Table_0 > tbody > tr > td.sorting_1 > a').click()
 
-    energy_avg_all_steps = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(2)").text_content()
+    energy_avg_all_steps = page.locator("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(2)").text_content()
     assert energy_avg_all_steps.strip() == '14.3 J (± 72.61%)'
 
-    time_avg_all_steps = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(3)").text_content()
+    time_avg_all_steps = page.locator("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(3)").text_content()
     assert time_avg_all_steps.strip() == '6.4s (± 59.48%)'
 
-    cpu_avg_all_steps = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(4)").text_content()
+    cpu_avg_all_steps = page.locator("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(4)").text_content()
     assert cpu_avg_all_steps.strip() == '38.6% (± 36.31%%)'
 
-    grid_all_steps = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(5)").text_content()
+    grid_all_steps = page.locator("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(5)").text_content()
     assert grid_all_steps.strip() == '494.2 gCO2/kWh (± 5.16%)'
 
-    carbon_all_steps = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(6)").text_content()
+    carbon_all_steps = page.locator("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(6)").text_content()
     assert carbon_all_steps.strip() == '0.008 gCO2e (± 71.27%)'
 
-    count_all_steps = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(7)").text_content()
+    count_all_steps = page.locator("#label-stats-table-avg > tr:nth-child(1) > td:nth-child(7)").text_content()
     assert count_all_steps.strip() == '10'
 
 
 
-    energy_avg_single = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(2)").text_content()
+    energy_avg_single = page.locator("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(2)").text_content()
     assert energy_avg_single.strip() == '4.46 J (± 10.96%)'
 
-    time_avg_single = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(3)").text_content()
+    time_avg_single = page.locator("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(3)").text_content()
     assert time_avg_single.strip() == '2.8s (± 15.97%)'
 
-    cpu_avg_single = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(4)").text_content()
+    cpu_avg_single = page.locator("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(4)").text_content()
     assert cpu_avg_single.strip() == '27.6% (± 41.83%%)'
 
-    grid_single = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(5)").text_content()
+    grid_single = page.locator("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(5)").text_content()
     assert grid_single.strip() == '494.2 gCO2/kWh (± 5.47%)'
 
-    carbon_single = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(6)").text_content()
+    carbon_single = page.locator("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(6)").text_content()
     assert carbon_single.strip() == '0.0026 gCO2e (± 9.83%)'
 
-    count_single = page.wait_for_selector("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(7)").text_content()
+    count_single = page.locator("#label-stats-table-avg > tr:nth-child(2) > td:nth-child(7)").text_content()
     assert count_single.strip() == '5'
 
 
@@ -108,49 +108,49 @@ def test_stats():
     new_page.set_default_timeout(2_000)
 
     # open details
-    new_page.wait_for_selector('div[data-tab="[RUNTIME]"] .ui.accordion a').click()
-    new_page.wait_for_selector('a.step[data-tab="[RUNTIME]"]').click()
+    new_page.locator('div[data-tab="[RUNTIME]"] .ui.accordion a').click()
+    new_page.locator('a.step[data-tab="[RUNTIME]"]').click()
 
-    energy_value = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.four.cards.stackable > div.ui.card.machine-energy > div > div.description > div.ui.fluid.mini.statistic > div > span").text_content()
-    phase_duration = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.four.cards.stackable > div.ui.card.phase-duration > div > div.description > div.ui.fluid.mini.statistic > div > span").text_content()
+    energy_value = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.four.cards.stackable > div.ui.card.machine-energy > div > div.description > div.ui.fluid.mini.statistic > div > span").text_content()
+    phase_duration = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.four.cards.stackable > div.ui.card.phase-duration > div > div.description > div.ui.fluid.mini.statistic > div > span").text_content()
 
 
     assert energy_value.strip() == '76.10'
     assert phase_duration.strip() == '5.20'
 
     # fetch time series
-    new_page.wait_for_selector('button#fetch-time-series').click()
+    new_page.locator('button#fetch-time-series').click()
 
     # expand charts
-    new_page.wait_for_selector('div#chart-container .statistics-chart-card button.toggle-width')
+    new_page.locator('div#chart-container .statistics-chart-card button.toggle-width')
     for el in new_page.locator('div#chart-container .statistics-chart-card button.toggle-width').all():
         el.click()
 
-    chart_label = new_page.wait_for_selector("#chart-container > div:nth-child(3) > div > div.ui.left.floated.chart-title").text_content()
+    chart_label = new_page.locator("#chart-container > div:nth-child(3) > div > div.ui.left.floated.chart-title").text_content()
     assert chart_label.strip() == 'CPU % via procfs'
 
 
-    first_metric = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(1)").text_content()
+    first_metric = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(1)").text_content()
     assert first_metric.strip() == 'CPU Energy (Package)'
 
-    first_value = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(6)").text_content()
+    first_value = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(6)").text_content()
     assert first_value.strip() == '45.05'
 
-    first_unit = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(7)").text_content()
+    first_unit = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(7)").text_content()
     assert first_unit.strip() == 'J'
 
 
     # click on baseline
-    new_page.wait_for_selector('a.step[data-tab="[BASELINE]"]').click()
-    new_page.wait_for_selector('div[data-tab="[BASELINE]"] .ui.accordion a').click()
+    new_page.locator('a.step[data-tab="[BASELINE]"]').click()
+    new_page.locator('div[data-tab="[BASELINE]"] .ui.accordion a').click()
 
-    first_metric = new_page.wait_for_selector("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(1)").text_content()
+    first_metric = new_page.locator("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(1)").text_content()
     assert first_metric.strip() == 'CPU Energy (Package)'
 
-    first_value = new_page.wait_for_selector("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(6)").text_content()
+    first_value = new_page.locator("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(6)").text_content()
     assert first_value.strip() == '9.69'
 
-    first_unit = new_page.wait_for_selector("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(7)").text_content()
+    first_unit = new_page.locator("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(7)").text_content()
     assert first_unit.strip() == 'J'
 
     new_page.close()
@@ -159,57 +159,57 @@ def test_stats():
 def test_repositories_and_compare():
     page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
     page.get_by_role("link", name="Repositories").click()
-    page.wait_for_selector('.ui.accordion div.title').click()
-    page.wait_for_selector('.dataTables_info') # wait for accordion to fetch XHR and open
+    page.locator('.ui.accordion div.title').click()
+    page.locator('.dataTables_info') # wait for accordion to fetch XHR and open
 
     elements = page.query_selector_all("input[type=checkbox]")  # Replace with your selector
     for element in elements:
         element.click()
     with context.expect_page() as new_page_info:
-        page.wait_for_selector('#compare-button').click()
+        page.locator('#compare-button').click()
 
     new_page = new_page_info.value
     new_page.set_default_timeout(5_000)
 
-    comparison_type = new_page.wait_for_selector('#run-data-top > tbody:nth-child(1) > tr > td:nth-child(2)').text_content()
+    comparison_type = new_page.locator('#run-data-top > tbody:nth-child(1) > tr > td:nth-child(2)').text_content()
     assert comparison_type == 'Repeated Run'
 
-    runs_compared = new_page.wait_for_selector('#run-data-top > tbody:nth-child(2) > tr > td:nth-child(2)').text_content()
+    runs_compared = new_page.locator('#run-data-top > tbody:nth-child(2) > tr > td:nth-child(2)').text_content()
     assert runs_compared == '5'
 
     # open details
-    new_page.wait_for_selector('a.step[data-tab="[RUNTIME]"]').click()
-    new_page.wait_for_selector('div[data-tab="[RUNTIME]"] .ui.accordion a').click()
+    new_page.locator('a.step[data-tab="[RUNTIME]"]').click()
+    new_page.locator('div[data-tab="[RUNTIME]"] .ui.accordion a').click()
 
 
 
-    first_metric = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(2) > td:nth-child(1)").text_content()
+    first_metric = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(2) > td:nth-child(1)").text_content()
     assert first_metric.strip() == 'CPU Power (Package)'
 
-    first_value = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(2) > td:nth-child(6)").text_content()
+    first_value = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(2) > td:nth-child(6)").text_content()
     assert first_value.strip() == '8.50'
 
-    first_unit = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(2) > td:nth-child(7)").text_content()
+    first_unit = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(2) > td:nth-child(7)").text_content()
     assert first_unit.strip() == 'W'
 
-    first_stddev = new_page.wait_for_selector("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(2) > td:nth-child(8)").text_content()
+    first_stddev = new_page.locator("#runtime-steps > div.ui.bottom.attached.active.tab.segment > div.ui.segment.secondary > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(2) > td:nth-child(8)").text_content()
     assert first_stddev.strip() == '± 2.85%'
 
 
     # click on baseline
-    new_page.wait_for_selector('a.step[data-tab="[BASELINE]"]').click()
-    new_page.wait_for_selector('div[data-tab="[BASELINE]"] .ui.accordion a').click()
+    new_page.locator('a.step[data-tab="[BASELINE]"]').click()
+    new_page.locator('div[data-tab="[BASELINE]"] .ui.accordion a').click()
 
-    first_metric = new_page.wait_for_selector("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(1)").text_content()
+    first_metric = new_page.locator("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(1)").text_content()
     assert first_metric.strip() == 'CPU Energy (Package)'
 
-    first_value = new_page.wait_for_selector("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(6)").text_content()
+    first_value = new_page.locator("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(6)").text_content()
     assert first_value.strip() == '9.21'
 
-    first_unit = new_page.wait_for_selector("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(7)").text_content()
+    first_unit = new_page.locator("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(7)").text_content()
     assert first_unit.strip() == 'J'
 
-    first_stddev = new_page.wait_for_selector("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(8)").text_content()
+    first_stddev = new_page.locator("#main > div.ui.tab.attached.segment.secondary.active > phase-metrics > div.ui.accordion > div.content.active > table > tbody > tr:nth-child(1) > td:nth-child(8)").text_content()
     assert first_stddev.strip() == '± 13.53%'
 
     new_page.close()
@@ -244,11 +244,11 @@ def test_status():
     page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
     page.get_by_role("link", name="Status").click()
 
-    machine_name = page.wait_for_selector('#machines-table > tbody > tr:nth-child(1) > td:nth-child(2)').text_content()
+    machine_name = page.locator('#machines-table > tbody > tr:nth-child(1) > td:nth-child(2)').text_content()
     assert machine_name.strip() == 'Local machine'
 
 
-    awaiting_info = page.wait_for_selector('#machines-table > tbody > tr:nth-child(1) > td:nth-child(10)').text_content()
+    awaiting_info = page.locator('#machines-table > tbody > tr:nth-child(1) > td:nth-child(10)').text_content()
     assert awaiting_info.strip() == 'awaiting info'
 
 
@@ -258,17 +258,17 @@ def test_settings():
     page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
     page.get_by_role("link", name="Settings").click()
 
-    energy_display = page.wait_for_selector('#energy-display').text_content()
+    energy_display = page.locator('#energy-display').text_content()
     assert energy_display.strip() == 'Currently showing Joules'
 
 
-    units_display = page.wait_for_selector('#units-display').text_content()
+    units_display = page.locator('#units-display').text_content()
     assert units_display.strip() == 'Currently showing imperial units'
 
 
-    fetch_time_series_display = page.wait_for_selector('#fetch-time-series-display').text_content()
+    fetch_time_series_display = page.locator('#fetch-time-series-display').text_content()
     assert fetch_time_series_display.strip() == 'Currently not fetching time series by default'
 
 
-    time_series_avg_display = page.wait_for_selector('#time-series-avg-display').text_content()
+    time_series_avg_display = page.locator('#time-series-avg-display').text_content()
     assert time_series_avg_display.strip() == 'Currently not showing AVG in time series'
