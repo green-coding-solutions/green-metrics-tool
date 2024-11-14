cb26e2c986e5c2332687fb3612aff68b0bad2bec
Arne Tarara <arne@datafuse.de>
2024-11-14 11:15:39 +0100

    Added temporary rescale guard clause

--- api/api_helpers.py
+++ api/api_helpers.py
@@ -56,7 +56,12 @@ def store_artifact(artifact_type: Enum, key:str, data, ex=2592000):
 # The same unit for proper comparison!
 #
 def rescale_energy_value(value, unit):
-    # We only expect values to be mJ for energy!
+    if unit == 'mJ':
+        value = value * 1_000
+        unit = 'uJ'
+
+    # We only expect values to be uJ for energy in the future. Changing values now temporarily.
+    # TODO: Refactor this once all data in the DB is uJ
     if unit != 'uJ' and not unit.startswith('ugCO2e/'):
         raise RuntimeError('Unexpected unit occured for energy rescaling: ', unit)
 
