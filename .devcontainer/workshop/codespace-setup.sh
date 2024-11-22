415542711dc8c965248d1883c464fc3f7fc75b44
Arne Tarara <arne@datafuse.de>
2024-11-22 21:29:13 +0100

    Activating ports correctly + enabling XGboost manually

--- .devcontainer/workshop/codespace-setup.sh
+++ .devcontainer/workshop/codespace-setup.sh
@@ -15,9 +15,11 @@ python3 -m pip install -r /workspaces/green-metrics-tool/metric_providers/psu/en
 
 # make edits to ports so we can use 9143 to access front end
 sed -i 's/listen \[::\]:9142;/listen [::]:9143;/; s/listen 9142;/listen 9143;/' /workspaces/green-metrics-tool/docker/nginx/frontend.conf
-sed -i '/green-coding-nginx:/,/green-coding-gunicorn:/ s/\(- 9142:80\)/- 9142:9142\n      - 9143:9143/' /workspaces/green-metrics-tool/docker/compose.yml
+sed -i 's/- 9142:9142/- 9142:9142\n      - 9143:9143/' /workspaces/green-metrics-tool/docker/compose.yml
 
-python3 /workspaces/green-metrics-tool/disable_metric_providers.py --categories GPU RAPL Machine Sensors Debug --providers NetworkIoCgroupContainerProvider NetworkConnectionsProxyContainerProvider PsuEnergyAcSdiaMachineProvider
+# activate XGBoost provider with sane values for GitHub Codespaces
+sed -i 's/common:/common:\n      psu.energy.ac.xgboost.machine.provider.PsuEnergyAcXgboostMachineProvider:\n        resolution: 99\n        CPUChips: 1\n        HW_CPUFreq: 2800\n        CPUCores: 32\n        CPUThreads: 64\n        TDP: 270\n        HW_MemAmountGB: 256\n        VHost_Ratio: 0.03125\n/' /workspaces/green-metrics-tool/config.yml
+
 
 git clone https://github.com/green-coding-solutions/example-applications.git --depth=1 --single-branch /workspaces/green-metrics-tool/example-applications || true
 
