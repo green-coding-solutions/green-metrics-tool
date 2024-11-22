e9d756984c7ff56346cd1dcbdb494bec8ed0cdeb
David Kopp <git@davidkopp.de>
2024-11-22 08:17:03 +0100

    Ensure ports are set to public after Codespace restart

--- .devcontainer/on-start.sh
+++ .devcontainer/on-start.sh
@@ -11,3 +11,7 @@ if ! sudo grep -Fxq "$etc_hosts_line_1" /etc/hosts; then
 else
     echo "Entry was already present..."
 fi
+
+# Ensure that after a restart of the Codespace the ports are set to public again
+gh codespace ports visibility 9142:public -c $CODESPACE_NAME
+gh codespace ports visibility 9143:public -c $CODESPACE_NAME
