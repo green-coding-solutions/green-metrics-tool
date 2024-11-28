f1d3af8f95fe974609a4397ff151957b8301c472
Arne Tarara <arne@datafuse.de>
2024-11-28 20:07:29 +0100

    Self-adding GMT Overhead to the monitored cgroups

--- metric_providers/cpu/utilization/cgroup/system/provider.py
+++ metric_providers/cpu/utilization/cgroup/system/provider.py
@@ -12,7 +12,19 @@ class CpuUtilizationCgroupSystemProvider(BaseMetricProvider):
             current_dir=os.path.dirname(os.path.abspath(__file__)),
             skip_check = skip_check,
         )
-        self._cgroups = cgroups
+        if cgroups:
+            self._cgroups = cgroups.copy() # because it is a frozen dict, we need to copy
+        else:
+            self._cgroups = {}
+
+        # we also find the cgroup that the GMT process belongs to. It will be a user session
+        current_pid = os.getpid()
+        with open(f"/proc/{current_pid}/cgroup", 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            if found_cgroups := len(lines) != 1:
+                raise RuntimeError(f"Could not find GMT\'s own cgroup or found too many. Amount: {found_cgroups}")
+            cgroup_name = lines[0].split('/')[-1].strip()
+            self._cgroups[cgroup_name] = 'GMT Overhead'
 
     def start_profiling(self, containers=None):
         # we hook here into the mechanism that can supply container names to the parent function
@@ -25,8 +37,8 @@ class CpuUtilizationCgroupSystemProvider(BaseMetricProvider):
             return df
 
         df['detail_name'] = df.container_id
-        for container_id in containers:
-            df.loc[df.detail_name == container_id, 'detail_name'] = containers[container_id]['name']
+        for container_id in self._cgroups:
+            df.loc[df.detail_name == container_id, 'detail_name'] = self._cgroups[container_id]
         df = df.drop('container_id', axis=1)
 
         return df
