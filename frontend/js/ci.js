786ca2e8db9c9406e4732dfcc97f5d4a2612cc00
Arne Tarara <arne@datafuse.de>
2024-12-23 15:24:39 +0100

    Refactorings

--- frontend/js/ci.js
+++ frontend/js/ci.js
@@ -284,9 +284,7 @@ const getBadges = async (repo, branch, workflow_id) => {
     }
 }
 
-const getMeasurementsAndStats = async (repo, branch, workflow_id) => {
-    const start_date = dateToYMD(new Date($('#rangestart input').val()), short=true);
-    const end_date = dateToYMD(new Date($('#rangeend input').val()), short=true);
+const getMeasurementsAndStats = async (repo, branch, workflow_id, start_date, end_date) => {
 
     const query_string=`repo=${repo}&branch=${branch}&workflow=${workflow_id}&start_date=${start_date}&end_date=${end_date}`;
     const [measurements, stats] = await Promise.all([
@@ -294,44 +292,79 @@ const getMeasurementsAndStats = async (repo, branch, workflow_id) => {
         makeAPICall(`/v1/ci/stats?${query_string}`)
     ]);
 
-    history.pushState(null, '', `${window.location.origin}${window.location.pathname}?repo=${repo}&branch=${branch}&workflow=${workflow_id}&start_date=${start_date}&end_date=${end_date}`); // replace URL to bookmark!
-
     return [measurements, stats];
 }
 
-const bindRefreshButton = (repo, branch, workflow_id, chart_instance) => {
-    // When the user selects a subset of the measurement data via the date-picker
-    $('#submit').on('click', async function () {
+const refreshView = async (repo, branch, workflow_id, chart_instance) => {
 
-        let measurements = null;
-        let stats = null;
-        try {
-            [measurements, stats] = await getMeasurementsAndStats(repo, branch, workflow_id); // iterates I
+    const start_date = dateToYMD(new Date($('#rangestart input').val()), short=true);
+    const end_date = dateToYMD(new Date($('#rangeend input').val()), short=true);
 
-        } catch (err) {
-            showNotification('Could not get data from API', err);
-            return; // abort
-        }
+    let measurements = null;
+    let stats = null;
+    try {
+        [measurements, stats] = await getMeasurementsAndStats(repo, branch, workflow_id, start_date, end_date); // iterates I
 
-        if (document.querySelector('#run-details-table').style.display != 'none') {
-            displayRunDetailsTable(measurements.data, repo);
-        }
+    } catch (err) {
+        showNotification('Could not get data from API', err);
+        return; // abort
+    }
 
-        displayStatsTable(stats.data); //iterates II
+    history.pushState(null, '', `${window.location.origin}${window.location.pathname}?repo=${repo}&branch=${branch}&workflow=${workflow_id}&start_date=${start_date}&end_date=${end_date}`); // replace URL to bookmark!
 
-        displayGraph(chart_instance, measurements.data)
+    const source = measurements.data[0][8];
+    let workflow_name = measurements.data[0][10];
+    populateRunInfos(repo, branch, source, workflow_name, workflow_id)
 
-        chart_instance.off('legendselectchanged') // remove
-        // we need to re-bind the handler here and can also not really refactor that
-        // without using a global variable. echarts .on does not allow to pass data to the handler
-        chart_instance.on('legendselectchanged', function (params) {
-            // get list of all legends that are on
-            const selectedLegends = params.selected;
-            const filteredMeasurements = measurements.data.filter(measurement => selectedLegends[measurement[4]]);
-            // displayStatsTable(filteredMeasurements); -- this does not work anymore, as we are now fetching the data from the API
-            // this decision was done as the JS code was very unmaintainable and SQL was far more readable. Performance was even though. Rework this if functionality needed
-        });
+    $('#display-run-details-table').off('click');
+    $('#display-run-details-table').on('click', () => {
+        document.querySelector('#api-loader').style.display = '';
+        document.querySelector('#loader-question').remove();
+        setTimeout(() => {displayRunDetailsTable(measurements?.data, repo); document.querySelector('#api-loader').remove();}, 100); // we need to include a mini delay here, because otherwise the loader does not render, but is blocked by the hefty calculation
     });
+
+    if (document.querySelector('#run-details-table').style.display != 'none') {
+        displayRunDetailsTable(measurements.data, repo);
+    }
+
+    displayStatsTable(stats.data); //iterates II
+    displayGraph(chart_instance, measurements.data)
+
+    chart_instance.off('legendselectchanged');
+
+    chart_instance.on('legendselectchanged', (params) => {
+        // get list of all legends that are on
+        const selectedLegends = params.selected;
+        const filteredMeasurements = measurements.data.filter(measurement => selectedLegends[measurement[4]]);
+        // displayStatsTable(filteredMeasurements); -- this does not work anymore, as we are now fetching the data from the API
+        // this decision was done as the JS code was very unmaintainable and SQL was far more readable. Performance was even though. Rework this if functionality needed
+    });
+
+    setTimeout(function(){console.log("Resize"); window.dispatchEvent(new Event('resize'))}, 500);
+}
+
+const populateRunInfos = (repo, branch, source, workflow_name, workflow_id) => {
+
+    const ci_data_node = document.querySelector('#ci-data')
+    ci_data_node.insertAdjacentHTML('afterbegin', `<tr><td><strong>Branch:</strong></td><td>${branch}</td></tr>`)
+    ci_data_node.insertAdjacentHTML('afterbegin', `<tr><td><strong>Workflow ID:</strong></td><td>${workflow_id}</td></tr>`)
+
+    if (workflow_name == '' || workflow_name == null) {
+        workflow_name = workflow_id ;
+    }
+    ci_data_node.insertAdjacentHTML('afterbegin', `<tr><td><strong>Workflow:</strong></td><td>${escapeString(workflow_name)}</td></tr>`)
+
+    let repo_link = ''
+    if(source == 'github') {
+        repo_link = `https://github.com/${repo}`;
+    }
+    else if(source == 'gitlab') {
+        repo_link = `https://gitlab.com/${repo}`;
+    }
+
+    const repo_link_node = `<a href="${repo_link}" target="_blank">${repo}</a>`
+    ci_data_node.insertAdjacentHTML('afterbegin', `<tr><td><strong>Repository:</strong></td><td>${repo_link_node}</td></tr>`)
+
 }
 
 
@@ -342,10 +375,10 @@ $(document).ready((e) => {
 
         const url_params = getURLParams();
 
-        let branch = escapeString(url_params['branch']);
-        let repo = escapeString(url_params['repo']);
-        let workflow_id = escapeString(url_params['workflow']);
-        const ci_data_node = document.querySelector('#ci-data')
+        const branch = escapeString(url_params['branch']);
+        const repo = escapeString(url_params['repo']);
+        const workflow_id = escapeString(url_params['workflow']);
+
 
         if (repo == null || repo == '' || repo == 'null') {
             showNotification('No Repo', 'Repo parameter in URL is empty or not present. Did you follow a correct URL?');
@@ -360,67 +393,18 @@ $(document).ready((e) => {
             return;
         }
 
+
         const element = createChartContainer("#chart-container", "run-energy");
         const chart_instance = echarts.init(element);
 
-        bindRefreshButton(repo, branch, workflow_id, chart_instance)
-
-        ci_data_node.insertAdjacentHTML('afterbegin', `<tr><td><strong>Branch:</strong></td><td>${escapeString(url_params['branch'])}</td></tr>`)
-        ci_data_node.insertAdjacentHTML('afterbegin', `<tr><td><strong>Workflow ID:</strong></td><td>${escapeString(workflow_id)}</td></tr>`)
-
+        dateTimePicker(7, url_params);
         getBadges(repo, branch, workflow_id) // async
 
-        dateTimePicker(7, url_params);
+        refreshView(repo, branch, workflow_id, chart_instance)
 
-        let measurements = null;
-        let stats = null;
-        try {
-            [measurements, stats] = await getMeasurementsAndStats(repo, branch, workflow_id)
-        } catch (err) {
-            showNotification('Could not get data from API', err);
-            return; // abort
-        }
-
-        const source = measurements.data[0][8]
-        let workflow_name = measurements.data[0][10]
-
-        if (workflow_name == '' || workflow_name == null) {
-            workflow_name = workflow_id ;
-        }
-        ci_data_node.insertAdjacentHTML('afterbegin', `<tr><td><strong>Workflow:</strong></td><td>${escapeString(workflow_name)}</td></tr>`)
-
-        let repo_link = ''
-        if(source == 'github') {
-            repo_link = `https://github.com/${repo}`;
-        }
-        else if(source == 'gitlab') {
-            repo_link = `https://gitlab.com/${repo}`;
-        }
-
-        const repo_link_node = `<a href="${repo_link}" target="_blank">${repo}</a>`
-        ci_data_node.insertAdjacentHTML('afterbegin', `<tr><td><strong>Repository:</strong></td><td>${repo_link_node}</td></tr>`)
-
-
-        displayGraph(chart_instance, measurements.data) // iterates I (total: 2)
-
-        displayStatsTable(stats.data) // iterates II (total: 4)
-
-        document.querySelector('#display-run-details-table').addEventListener('click', () => {
-            document.querySelector('#api-loader').style.display = '';
-            document.querySelector('#loader-question').remove();
-            setTimeout(() => {displayRunDetailsTable(measurements.data, repo); document.querySelector('#api-loader').remove();}, 100); // we need to include a mini delay here, because otherwise the loader does not render, but is blocked by the hefty calculation
+        $('#submit').on('click', async function () {
+            refreshView(repo, branch, workflow_id, chart_instance)
         });
 
-
-        // On legend change, recalculate stats table
-        chart_instance.on('legendselectchanged', function (params) {
-            // get list of all legends that are on
-            const selectedLegends = params.selected;
-            const filteredMeasurements = measurements.data.filter(measurement => selectedLegends[measurement[4]]);
-            // displayStatsTable(filteredMeasurements); -- this does not work anymore, as we are now fetching the data from the API
-            // this decision was done as the JS code was very unmaintainable and SQL was far more readable. Performance was even though. Rework this if functionality needed
-        });
-
-        setTimeout(function(){console.log("Resize"); window.dispatchEvent(new Event('resize'))}, 500);
     })();
 });
