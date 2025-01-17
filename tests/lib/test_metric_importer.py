3db0dafc24d6d6d0d8e83532ae3dffc8d39808d2
Arne Tarara <arne@datafuse.de>
2025-01-17 19:30:46 +0800

    (Tests): fix

--- tests/lib/test_metric_importer.py
+++ tests/lib/test_metric_importer.py
@@ -14,13 +14,11 @@ def test_import_cpu_utilization_container():
 
     result = results[0]
     assert result[0] == len(measurement_lines)/2 # since we group, length mus be divided by groups
-    assert math.isclose(result[1], 3958.59401, rel_tol=1e-5), 'AVG value not in expected range'
+    assert math.isclose(result[1], 1985.44716, rel_tol=1e-5), 'AVG value not in expected range'
 
     result = results[1]
     assert result[0] == len(measurement_lines)/2 # since we group, length mus be divided by groups
-    assert math.isclose(result[1], 1985.44716, rel_tol=1e-5), 'AVG value not in expected range'
-
-
+    assert math.isclose(result[1], 3958.59401, rel_tol=1e-5), 'AVG value not in expected range'
 
 def test_import_machine_energy():
 
