1486b2b01a24e3342ecc714d9bd7f1ce2ab23817
David Kopp <git@davidkopp.de>
2025-03-12 22:22:07 +0100

    Fix access of the frontend to the test nginx container

--- tests/start-test-containers.sh
+++ tests/start-test-containers.sh
@@ -11,6 +11,11 @@ while getopts "d" opts; do
     esac
 done
 
+# The test nginx container is running on port 9143 instead of 9142.
+# So the frontend is able to access the correct nginx container, 
+# we temporarily change the port in the fronted config.
+sed -i 's/9142/9143/' ../frontend/js/helpers/config.js
+
 # docker compose with -f flag if force is true
 echo $detach
 
@@ -19,3 +24,6 @@ if [ "$detach" = true ] ; then
 else
     docker compose -f ../docker/test-compose.yml up --remove-orphans
 fi
+
+# Revert the change in the frontend config again.
+sed -i 's/9143/9142/' ../frontend/js/helpers/config.js
