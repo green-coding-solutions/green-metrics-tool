503086900404c5a06c3c4998f777ac2baf524b12
Arne Tarara <arne@datafuse.de>
2025-04-26 16:50:20 +0200

    Deleting old and outdated migrations

--- migrations/2023_09_01_network_intercepts.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE network_intercepts (
-    id SERIAL PRIMARY KEY,
-    project_id uuid NOT NULL REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE ,
-    time bigint NOT NULL,
-    connection_type text NOT NULL,
-    protocol text NOT NULL,
-    created_at timestamp with time zone DEFAULT now()
-);
