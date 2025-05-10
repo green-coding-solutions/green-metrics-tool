56350607eb10d98ed3d1cf54364028a60bd6a202
Arne Tarara <arne@datafuse.de>
2025-05-10 09:32:06 +0200

    Apdated paths

--- lib/install_shared.sh
+++ lib/install_shared.sh
@@ -281,22 +281,21 @@ function checkout_submodules() {
             git -C ee checkout $ee_branch
         fi
 
-        ln -sf ee/cron/delete_expired_data.py cron/delete_expired_data.py
-        ln -sf ee/cron/carbondb_copy_over_and_remove_duplicates.py cron/carbondb_copy_over_and_remove_duplicates.py
-        ln -sf ee/cron/carbondb_compress.py cron/carbondb_compress.py
-        ln -sf ee/tests/cron/test_carbondb_compress.py tests/cron/test_carbondb_compress.py
-        ln -sf ee/tests/frontend/test_frontend_ee.py tests/frontend/test_frontend_ee.py
-        ln -sf ee/tests/api/test_api_hog.py tests/api/test_api_hog.py
-        ln -sf ee/tests/api/hog_data.py tests/api/hog_data.py
-        ln -sf ee/tests/api/test_api_carbondb.py tests/api/test_api_carbondb.py
-        ln -sf ee/tests/cron/test_carbondb_compress.py tests/cron/test_carbondb_compress.py
-        ln -sf ee/tools/rebuild_carbondb.py tools/rebuild_carbondb.py
-        ln -sf ee/frontend/js/hog-details.js frontend/js/hog-details.js
-        ln -sf ee/frontend/js/carbondb.js frontend/js/carbondb.js
-        ln -sf ee/frontend/js/hog.js frontend/js/hog.js
-        ln -sf ee/frontend/hog-details.html frontend/hog-details.html
-        ln -sf ee/frontend/hog.html frontend/hog.html
-        ln -sf ee/frontend/carbondb.html frontend/carbondb.html
+        ln -sf ../ee/cron/delete_expired_data.py cron/delete_expired_data.py
+        ln -sf ../ee/cron/carbondb_copy_over_and_remove_duplicates.py cron/carbondb_copy_over_and_remove_duplicates.py
+        ln -sf ../ee/cron/carbondb_compress.py cron/carbondb_compress.py
+        ln -sf ../../ee/tests/cron/test_carbondb_compress.py tests/cron/test_carbondb_compress.py
+        ln -sf ../../ee/tests/frontend/test_frontend_ee.py tests/frontend/test_frontend_ee.py
+        ln -sf ../../ee/tests/api/test_api_hog.py tests/api/test_api_hog.py
+        ln -sf ../../ee/tests/api/test_api_carbondb.py tests/api/test_api_carbondb.py
+        ln -sf ../../ee/tests/cron/test_carbondb_compress.py tests/cron/test_carbondb_compress.py
+        ln -sf ../ee/tools/rebuild_carbondb.py tools/rebuild_carbondb.py
+        ln -sf ../../ee/frontend/js/hog-details.js frontend/js/hog-details.js
+        ln -sf ../../ee/frontend/js/carbondb.js frontend/js/carbondb.js
+        ln -sf ../../ee/frontend/js/hog.js frontend/js/hog.js
+        ln -sf ../ee/frontend/hog-details.html frontend/hog-details.html
+        ln -sf ../ee/frontend/hog.html frontend/hog.html
+        ln -sf ../ee/frontend/carbondb.html frontend/carbondb.html
     fi
 }
 
