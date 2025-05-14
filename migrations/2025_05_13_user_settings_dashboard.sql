fa947b1bcb15c390a5adbd03c36d20f89a85042b
Arne Tarara <arne@datafuse.de>
2025-05-14 07:35:40 +0200

    Updated migration to merge and add settings fields

--- migrations/2025_05_13_user_settings_dashboard.sql
+++ migrations/2025_05_13_user_settings_dashboard.sql
@@ -2,7 +2,29 @@ UPDATE users
 SET capabilities = jsonb_set(
     capabilities,
     '{user,updateable_settings}',
-    '["measurement.dev_no_sleeps","measurement.dev_no_optimizations","measurement.total_duration"]'::jsonb,
-    true -- Create the key if it doesn't exist
+    (
+        COALESCE(capabilities->'user'->'updateable_settings', '[]'::jsonb) ||
+        '["measurement.dev_no_sleeps","measurement.dev_no_optimizations"]'::jsonb
+    ),
+    true
 )
-WHERE user_id = 1;
+WHERE id = 1;
+
+
+UPDATE users
+SET capabilities = jsonb_set(
+    capabilities,
+    '{measurement,dev_no_sleeps}',
+    'false'::jsonb,
+    true
+)
+WHERE id = 1;
+
+UPDATE users
+SET capabilities = jsonb_set(
+    capabilities,
+    '{measurement,dev_no_optimizations}',
+    'false'::jsonb,
+    true
+)
+WHERE id = 1;
