c75d44ee4559a818dcf45c735f1f76357caefa63
Arne Tarara <arne@datafuse.de>
2025-05-18 15:55:59 +0200

    Sorting by created_at now

--- frontend/js/status.js
+++ frontend/js/status.js
@@ -98,7 +98,7 @@ $(document).ready(function () {
                 { data: 9, title: 'Updated at', render: (el) => el == null ? '-' : dateToYMD(new Date(el)) },
             ],
             deferRender: true,
-            order: [[8, 'desc']] // API also orders, but we need to indicate order for the user
+            order: [[7, 'desc']] // API also orders, but we need to indicate order for the user
         });
 
 
