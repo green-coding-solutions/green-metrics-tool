565f41400a8e6c2043d7546224002008cabab180
Arne Tarara <arne@datafuse.de>
2025-05-19 07:14:12 +0200

    Changed resolution to sampling_rate [skip ci]

--- metric_providers/gpu/energy/nvidia/nvml/component/provider.py
+++ metric_providers/gpu/energy/nvidia/nvml/component/provider.py
@@ -3,11 +3,11 @@ import os
 from metric_providers.base import BaseMetricProvider
 
 class GpuEnergyNvidiaNvmlComponentProvider(BaseMetricProvider):
-    def __init__(self, resolution, skip_check=False):
+    def __init__(self, sampling_rate, skip_check=False):
         super().__init__(
             metric_name='gpu_energy_nvidia_nvml_component',
             metrics={'time': int, 'value': int, 'card_model': str},
-            resolution=resolution,
+            sampling_rate=sampling_rate,
             unit='uJ',
             current_dir=os.path.dirname(os.path.abspath(__file__)),
             skip_check=skip_check,
