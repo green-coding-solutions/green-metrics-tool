66bc5bda935a7261839d10f74e6e123f3569731d
Arne Tarara <arne@datafuse.de>
2025-05-22 08:43:06 +0200

    Allowing filter by failed runs

--- api/scenario_runner.py
+++ api/scenario_runner.py
@@ -199,7 +199,7 @@ def old_v1_runs_endpoint():
 
 # A route to return all of the available entries in our catalog.
 @router.get('/v2/runs')
-async def get_runs(uri: str | None = None, branch: str | None = None, machine_id: int | None = None, machine: str | None = None, filename: str | None = None, job_id: int | None = None, limit: int | None = 50, uri_mode = 'none', user: User = Depends(authenticate)):
+async def get_runs(uri: str | None = None, branch: str | None = None, machine_id: int | None = None, machine: str | None = None, filename: str | None = None, job_id: int | None = None, failed: bool | None = None, limit: int | None = 50, uri_mode = 'none', user: User = Depends(authenticate)):
 
     query = '''
             SELECT r.id, r.name, r.uri, r.branch, r.created_at, r.invalid_run, r.filename, r.usage_scenario_variables, m.description, r.commit_hash, r.end_measurement, r.failed, r.machine_id
@@ -237,6 +237,10 @@ async def get_runs(uri: str | None = None, branch: str | None = None, machine_id
     if job_id:
         query = f"{query} AND r.job_id = %s \n"
         params.append(job_id)
+
+    if failed is not None:
+        query = f"{query} AND r.failed = %s \n"
+        params.append(bool(failed))
 
     query = f"{query} ORDER BY r.created_at DESC"
 
