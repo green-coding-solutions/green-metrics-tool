66de63459a6e7123fd24b643749c690be762c350
Arne Tarara <arne@datafuse.de>
2025-06-14 12:41:56 +0200

    turning ntp off takes a short while

--- tools/cluster/cleanup_original.py
+++ tools/cluster/cleanup_original.py
@@ -41,6 +41,7 @@ if 'System clock synchronized: yes' not in ntp_status or 'NTP service: active' n
     raise RuntimeError('System clock could not be synchronized', ntp_status)
 
 result = subprocess.check_output(['sudo', 'timedatectl', 'set-ntp', 'false']) # we want NTP always off in clusters
+time.sleep(2)
 ntp_status = subprocess.check_output(['timedatectl', '-a'], encoding='UTF-8')
 if 'System clock synchronized: yes' not in ntp_status:
     raise RuntimeError('System clock synchronization could not be synchronized', ntp_status)
