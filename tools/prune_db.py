383e259637d74511cbfb03f5dba00dd47a6b6b48
David Kopp <git@davidkopp.de>
2025-06-20 18:48:39 +0200

    Delete runs that are either failed or have no end measurement date

--- tools/prune_db.py
+++ tools/prune_db.py
@@ -31,5 +31,5 @@ if __name__ == '__main__':
         print("This will remove all runs that have not ended, which includes failed ones, but also possibly running, so be sure no measurement is currently active. Continue? (y/N)")
         answer = sys.stdin.readline()
         if answer.strip().lower() == 'y':
-            DB().query('DELETE FROM runs WHERE failed = TRUE')
+            DB().query('DELETE FROM runs WHERE failed = TRUE OR end_measurement IS NULL')
             print("Done")
