7e24c3ff956b6389c404b368d8e317176bc890c6
Arne Tarara <arne@datafuse.de>
2025-10-16 13:48:40 +0200

    Fixed syntax warning for invalid escape

--- lib/scenario_runner.py
+++ lib/scenario_runner.py
@@ -414,7 +414,7 @@ class ScenarioRunner:
                 nodes = loader.get_constructed_nodes(node)
 
                 if not re.fullmatch(r'gmt-playwright-(?:with-cache-)?(v\d+.\d+.\d+)\.yml', nodes[0]):
-                    raise ValueError(f"You tried include unallowed files with !include-gmt-helper function. Included files must conform to regex gmt-playwright-(?:with-cache-)?(v\d+.\d+.\d+)\.yml but actually is {nodes[0]}")
+                    raise ValueError(f"You tried include unallowed files with !include-gmt-helper function. Included files must conform to regex gmt-playwright-(?:with-cache-)?(v\\d+.\\d+.\\d+)\\.yml but actually is {nodes[0]}")
 
                 filename = runner_join_paths(f"{GMT_ROOT_DIR}/templates/partials/", nodes[0], force_path_as_root=True, force_path_in_repo=False)
 
