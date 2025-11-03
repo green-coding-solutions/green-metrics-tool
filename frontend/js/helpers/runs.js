07c9f574995d1b9c939d3fc342f649b6bd3a8ae1
Arne Tarara <arne@datafuse.de>
2025-11-03 19:45:31 +0100

    (fix): String was double quoted and number also falsely quoted

--- frontend/js/helpers/runs.js
+++ frontend/js/helpers/runs.js
@@ -264,7 +264,7 @@ const getRunsTable = async (el, url, include_uri=true, include_button=true, sear
         title: '<i class="icon calendar"></i>Last run</th>',
         render: function(el, type, row) {
             if (el == null) return '-';
-            let usage_scenario_variables = Object.entries(row[7]).map(([k, v]) => typeof(v) == 'number' ? `"${k}": ${JSON.stringify(v)}` : `"${k}": "${JSON.stringify(v)}"`).join(', ')
+            let usage_scenario_variables = Object.entries(row[7]).map(([k, v]) => typeof(v) == 'number' ? `"${k}": ${v}` : `"${k}": ${JSON.stringify(v)}`).join(', ')
             usage_scenario_variables = `{${usage_scenario_variables}}`
 
             return `${dateToYMD(new Date(el))}<br><a href="/timeline.html?uri=${encodeURIComponent(row[2])}&amp;branch=${encodeURIComponent(row[3])}&amp;machine_id=${row[12]}&amp;filename=${encodeURIComponent(row[6])}&amp;usage_scenario_variables=${encodeURIComponent(usage_scenario_variables)}&amp;metrics=key" class="ui teal horizontal label  no-wrap"><i class="ui icon clock"></i>History &nbsp;</a>`;
