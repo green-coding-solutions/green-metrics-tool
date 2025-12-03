6f7c9d14c5e478cf9e3fb26dfcfb78c792f02d61
Arne Tarara <arne@datafuse.de>
2025-12-03 11:27:09 +0100

    Open Sourced CarbonDB and PowerHOG (#1441)
    * Open Sourced CarbonDB and PowerHOG
    
    * Setting carbondb and powerhog activate default for tests
    
    * Removed unnecessary demo data ee imports
    
    * (fix): Made tests more robust with actively closing context and having sleeker import data logic
    
    * Fixes hog error
    
    * Changed to our server which should stay the same location
    
    ---------
    
    Co-authored-by: Didi Hoffmann <39629+ribalba@users.noreply.github.com>
    Co-authored-by: Didi Hoffmann <didi@ribalba.de>

---
+++ cron/carbondb_copy_over_and_remove_duplicates.py
@@ -0,0 +1,174 @@
+import faulthandler
+faulthandler.enable()  # will catch segfaults and write to stderr
+
+import os
+
+from lib.global_config import GlobalConfig
+from lib.db import DB
+from lib import error_helpers
+
+# TODO: Currently this assumes all data is coming from Germany!
+# We need need a mean to discern where the connection from Power HOG was coming from.
+# Either we supply the power region, ip address or the coordinates
+
+# TODO 2: This is currently only for PowerHOG. Waiting till other services fail so we can test and add it there too ...
+def backfill_missing_carbon_intensity():
+    query = '''
+        UPDATE hog_simplified_measurements as hsm
+        SET grid_intensity_cog = (SELECT ci.data->>'carbonIntensity'
+            FROM carbon_intensity as ci
+            WHERE ci.data->>'zone' = 'DE'
+            ORDER BY ABS(EXTRACT(EPOCH FROM (hsm.created_at - ci.created_at::timestamp)))
+            LIMIT 1)::int
+        WHERE hsm.grid_intensity_cog IS NULL;
+    '''
+    DB().query(query)
+
+    query = '''
+        UPDATE hog_simplified_measurements
+        SET operational_carbon_ug = ((combined_energy_uj::DOUBLE PRECISION)/1e3/3600/1000)*grid_intensity_cog
+        WHERE operational_carbon_ug IS NULL;
+    '''
+    DB().query(query)
+
+def copy_over_power_hog(interval=30):
+    params = []
+    query = '''
+        INSERT INTO carbondb_data_raw
+            ("type", "project", "machine", "source", "tags","time","energy_kwh","carbon_kg","carbon_intensity_g","latitude","longitude","ip_address","user_id","created_at")
+
+            SELECT
+                'machine.desktop',
+                'Not-Set',
+                machine_uuid,
+                'Power HOG',
+                '{}',
+                EXTRACT(EPOCH FROM created_at) * 1e6,
+                (combined_energy_uj::DOUBLE PRECISION)/1e6/3600/1000, -- to get to kWh
+                (operational_carbon_ug::DOUBLE PRECISION)/1e9 + (embodied_carbon_ug/1e9), -- to get to kg
+                -1,  -- (carbon_intensity_g) there is no need for this column for further processing
+                NULL,  -- (latitude) there is no need for this column for further processing
+                NULL,  -- (longitude) there is no need for this column for further processing
+                NULL,
+                user_id,
+                created_at
+            FROM hog_simplified_measurements
+    '''
+    if interval:
+        query = f"{query} WHERE created_at >= CURRENT_DATE - make_interval(days => %s)"
+        params.append(interval)
+
+    DB().query(query, params=params)
+
+
+def copy_over_eco_ci(interval=1):
+    params = []
+    query = '''
+        INSERT INTO carbondb_data_raw
+            ("type", "project", "machine", "source", "tags","time","energy_kwh","carbon_kg","carbon_intensity_g","latitude","longitude","ip_address","user_id","created_at")
+
+            SELECT
+                filter_type,
+                filter_project,
+                filter_machine,
+                'Eco CI',
+                filter_tags,
+                EXTRACT(EPOCH FROM created_at) * 1e6,
+                (energy_uj::DOUBLE PRECISION)/1e6/3600/1000, -- to get to kWh
+                (carbon_ug::DOUBLE PRECISION)/1e9, -- to get to kg
+                -1,  -- (carbon_intensity_g) there is no need for this column for further processing
+                NULL,  -- (latitude) there is no need for this column for further processing
+                NULL,  -- (longitude) there is no need for this column for further processing
+                ip_address,
+                user_id,
+                created_at
+            FROM ci_measurements
+    '''
+    if interval:
+        query = f"{query} WHERE created_at >= CURRENT_DATE - make_interval(days => %s)"
+        params.append(interval)
+
+    DB().query(query, params=params)
+
+def copy_over_scenario_runner(interval=30):
+    params = []
+    query = '''
+        INSERT INTO carbondb_data_raw
+            ("type", "project", "machine", "source", "tags","time","energy_kwh","carbon_kg","carbon_intensity_g","latitude","longitude","ip_address","user_id","created_at")
+            SELECT
+                'machine.server' as type,
+                'ScenarioRunner' as project,
+                m.description,
+                'ScenarioRunner',
+                ARRAY[]::text[] as tags ,
+                EXTRACT(EPOCH FROM r.created_at) * 1e6 as time,
+
+                -- we do these two queries as subselects as if they were left joins they will blow up the table whenever we relax the condition that only one metric with same name may exist
+                COALESCE((SELECT SUM(value::DOUBLE PRECISION) FROM phase_stats as p WHERE p.run_id = r.id AND p.unit = 'uJ' AND p.phase != '004_[RUNTIME]' AND p.metric LIKE '%%_energy_%%_machine')/1e6/3600/1000, 0) as energy_kwh,
+                COALESCE((SELECT SUM(value::DOUBLE PRECISION) FROM phase_stats as p2 WHERE p2.run_id = r.id AND p2.unit = 'ug' AND p2.phase != '004_[RUNTIME]' AND p2.metric LIKE '%%_carbon_%%_machine')/1e9, 0) as carbon_kg,
+
+                -1, -- there is no need for this column for further processing
+                NULL,  -- there is no need for this column for further processing
+                NULL,  -- there is no need for this column for further processing
+                NULL,
+                r.user_id,
+                r.created_at
+            FROM runs as r
+            -- we do LEFT JOIN as we do not want to silent skip data. If a column gets NULL it will fail
+            LEFT JOIN machines as m ON m.id = r.machine_id
+    '''
+    if interval:
+        query = f"{query} WHERE r.created_at >= CURRENT_DATE - make_interval(days => %s)"
+        params.append(interval)
+
+    query = f"{query} GROUP BY r.id, m.description"
+
+    DB().query(query, params=params)
+
+
+def validate_table_constraints():
+    data = DB().fetch_all('''
+        SELECT
+            column_name,
+            is_nullable
+        FROM
+            information_schema.columns
+        WHERE
+            table_name = 'carbondb_data_raw'
+            AND column_name IN ('user_id', 'time', 'energy_kwh', 'carbon_kg', 'carbon_intensity_g', 'type', 'project', 'machine', 'source', 'tags')
+    ''')
+
+    for row in data:
+        if row[1] == 'YES':
+            raise RuntimeError(f"{row[0]} was NULL-able: {row[1]}. CarbonDB cannot remove duplicates.")
+
+
+def remove_duplicates():
+    validate_table_constraints() # since the query works only if columns are not null
+    DB().query('''
+        DELETE FROM carbondb_data_raw a
+        USING carbondb_data_raw b
+        WHERE
+            a.ctid < b.ctid
+            AND a.time = b.time
+            AND a.machine = b.machine
+            AND a.type = b.type
+            AND a.project = b.project
+            AND a.source = b.source
+            AND a.tags = b.tags
+            AND a.energy_kwh = b.energy_kwh
+            AND a.carbon_kg = b.carbon_kg
+            AND a.user_id = b.user_id;
+    ''')
+
+
+if __name__ == '__main__':
+    try:
+        GlobalConfig().override_config(config_location=f"{os.path.dirname(os.path.realpath(__file__))}/../../manager-config.yml")
+        backfill_missing_carbon_intensity()
+        copy_over_eco_ci()
+        copy_over_scenario_runner()
+        copy_over_power_hog()
+        remove_duplicates()
+    except Exception as exc: # pylint: disable=broad-except
+        error_helpers.log_error(f'Processing in {__file__} failed.', exception=exc, machine=GlobalConfig().config['machine']['description'])
---
+++ tests/frontend/test_frontend_carbondb.py
@@ -0,0 +1,144 @@
+import pytest
+
+from lib.global_config import GlobalConfig
+from lib.db import DB
+
+from tests import test_functions as Tests
+from playwright.sync_api import sync_playwright
+
+page = None
+context = None
+playwright = None
+browser = None
+
+API_URL = GlobalConfig().config['cluster']['api_url'] # will be pre-loaded with test-config.yml due to conftest.py
+
+## Reset DB only once after module
+#pylint: disable=unused-argument
+@pytest.fixture(autouse=True, scope='module')
+def setup_and_cleanup_module():
+    # before
+
+    global playwright #pylint: disable=global-statement
+    # start only one browser for whole file
+    playwright = sync_playwright().start()
+
+    Tests.import_demo_data()
+    yield
+
+    # after
+    playwright.stop()
+
+    Tests.reset_db()
+
+# will run after every test.
+# We must close the browser to clear localStorage
+@pytest.fixture(autouse=True)
+def setup_and_cleanup_test():
+    global page #pylint: disable=global-statement
+    global context #pylint: disable=global-statement
+    global browser #pylint: disable=global-statement
+
+    browser = playwright.firefox.launch()
+    context = browser.new_context(viewport={"width": 1920, "height": 5600})
+    page = context.new_page()
+    page.set_default_timeout(3_000)
+    yield
+    page.close()
+    context.close()
+    browser.close()
+
+def test_carbondb_display():
+
+
+    page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
+    page.locator("#menu").get_by_role("link", name="CarbonDB", exact=True).click()
+
+    carbondb_set_date_to_test_data_date(page)
+
+    page.locator('#carbondb-barchart-carbon-chart canvas').wait_for(timeout=3_000) # will wait for
+
+    total_carbon = page.locator('#total-carbon').text_content()
+    assert total_carbon.strip() == '1477.00'
+
+def test_carbondb_manual_add():
+
+    try:
+        DB().query('''
+            INSERT INTO carbondb_data(id, type,project,machine,source,tags,date,energy_kwh_sum,carbon_kg_sum,carbon_intensity_g_avg,record_count,user_id)
+            VALUES
+            (3000, 1,1,1,1,ARRAY[]::int[],E'2024-10-10',1.25e3,300,283,7,1);
+        ''')
+
+
+        page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
+        page.locator("#menu").get_by_role("link", name="CarbonDB", exact=True).click()
+
+#        page.screenshot(path="problem.png")
+
+        carbondb_set_date_to_test_data_date(page)
+
+        page.get_by_role("button", name="Refresh").click()
+
+        page.locator('#carbondb-barchart-carbon-chart canvas').wait_for(timeout=3_000) # will wait for
+
+        total_carbon = page.locator('#total-carbon').text_content()
+        assert total_carbon.strip() == '1777.00'
+
+    finally:
+        DB().query('DELETE FROM carbondb_data WHERE id = 3000;')
+
+
+def test_carbondb_display_xss_tags():
+
+    try:
+        DB().query('''
+            INSERT INTO carbondb_tags(id,tag,user_ids)
+            VALUES (999,'<script>alert(XSS);</script>','{1}');
+            INSERT INTO carbondb_data(id,type,project,machine,source,tags,date,energy_kwh_sum,carbon_kg_sum,carbon_intensity_g_avg,record_count,user_id)
+            VALUES
+            (3000,1,1,1,1,ARRAY[999],E'2024-10-10',1.25e3,300,283,7,1);
+        ''')
+
+
+        page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
+        page.locator("#menu").get_by_role("link", name="CarbonDB", exact=True).click()
+
+        page.locator('#show-filters').click()
+
+        all_tags = page.locator('#tags-include').locator("option").evaluate_all("options => options.map(option => option.textContent)")
+        assert '<script>alert(XSS);</script>' not in all_tags
+        assert '&lt;script&gt;alert(XSS);&lt;/script&gt;' in all_tags
+
+
+    finally:
+        DB().query('DELETE FROM carbondb_data WHERE id = 3000;')
+
+def test_carbondb_no_display_different_user():
+    Tests.insert_user(234, 'NO-CARBONDB')
+
+
+    page.goto(GlobalConfig().config['cluster']['metrics_url'] + '/index.html')
+    page.locator("#menu").get_by_role("link", name="Authentication", exact=True).click()
+
+    page.locator('#authentication-token').fill('NO-CARBONDB')
+    page.locator('#save-authentication-token').click()
+    page.locator('#token-details-message').wait_for(state='visible')
+
+    page.locator("#menu").get_by_role("link", name="CarbonDB", exact=True).click()
+
+    carbondb_set_date_to_test_data_date(page)
+
+    page.locator('#total-carbon').wait_for(state='hidden')
+    assert page.locator('#total-carbon').text_content().strip() == '--' # nothing to show
+
+    page.locator('#no-data-message').wait_for(state='visible')
+
+
+def carbondb_set_date_to_test_data_date(custom_page):
+    custom_page.wait_for_load_state("load") # ALL JS should be done
+    custom_page.locator("#show-filters").click()
+    custom_page.locator("input[name=range_start]").fill('01.01.2024')
+    custom_page.locator("input[name=range_end]").fill('2024-10-31')
+    custom_page.get_by_role("button", name="Refresh").click()
+    custom_page.wait_for_load_state("load") # ALL JS should be done
