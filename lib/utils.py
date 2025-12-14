0d18f2cf85b7a40b3dd50e8ad8ac98d8805c91b0
Arne Tarara <arne@datafuse.de>
2025-12-14 11:35:16 +0100

    Typo

--- lib/utils.py
+++ lib/utils.py
@@ -36,7 +36,7 @@ def freeze_dict(d):
         # Convert lists to tuples (to make them immutable)
         return tuple(freeze_dict(item) for item in d)
     if not hasattr(d, '__class__') and isinstance(d, object): # hasattr __class__ separates actual defined classes from builtins like str
-        raise RuntimeError(f"freeze_dict received object of type {type(d)} in it's config initalization. This is not expected and leads to issues as it cannot be made immutable!")
+        raise RuntimeError(f"freeze_dict received object of type {type(d)} in its config initalization. This is not expected and leads to issues as it cannot be made immutable!")
 
     return d
 
