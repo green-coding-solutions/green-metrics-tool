1aa2b103c2cb65102bbeb6c0d72761dcae7c2119
Arne Tarara <arne@datafuse.de>
2026-02-17 12:14:24 +0100

    API can now filter by start and end date [skip ci]

--- api/scenario_runner.py
+++ api/scenario_runner.py
@@ -300,7 +300,7 @@ def old_v1_runs_endpoint():
 
 # A route to return all of the available entries in our catalog.
 @router.get('/v2/runs')
-async def get_runs(name: str | None = None, uri: str | None = None, branch: str | None = None, machine_id: int | None = None, machine: str | None = None, filename: str | None = None, usage_scenario_variables: str | None = None, job_id: int | None = None, failed: bool | None = None, show_archived: bool | None = None, show_other_users: bool | None = None, limit: int | None = 50, uri_mode = 'none', user: User = Depends(authenticate)):
+async def get_runs(name: str | None = None, uri: str | None = None, branch: str | None = None, machine_id: int | None = None, machine: str | None = None, filename: str | None = None, usage_scenario_variables: str | None = None, job_id: int | None = None, failed: bool | None = None, show_archived: bool | None = None, show_other_users: bool | None = None, limit: int | None = 50, uri_mode = 'none', start_date: date | None = None, end_date: date | None = None, user: User = Depends(authenticate)):
 
     query = '''
             SELECT r.id, r.name, r.uri, r.branch, r.created_at,
@@ -366,7 +366,13 @@ async def get_runs(name: str | None = None, uri: str | None = None, branch: str 
     if show_archived is not True:
         query = f"{query} AND r.archived = False \n"
 
+    if start_date is not None:
+        query = f"{query} AND DATE(r.created_at) >= %s"
+        params.append(start_date)
 
+    if end_date is not None:
+        query = f"{query} AND DATE(r.created_at) <= %s"
+        params.append(end_date)
 
     query = f"{query} ORDER BY r.created_at DESC"
 
