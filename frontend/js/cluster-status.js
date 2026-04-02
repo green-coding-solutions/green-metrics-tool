5a2bad32d201f97b3fda8b845d0bcf4b0ea22e10
Arne Tarara <arne@datafuse.de>
2026-04-02 09:05:04 +0200

    Order machines by availability

--- frontend/js/cluster-status.js
+++ frontend/js/cluster-status.js
@@ -94,7 +94,7 @@ $(document).ready(function () {
                         $('#machine-configuration').modal('show');
                     })
             },
-            order: [] // empty means enforce no ordering. Use API ordering
+            order: [[2, 'asc']], // API also orders, but we need to indicate order for the user
         });
 
         $('#jobs-table').DataTable({
