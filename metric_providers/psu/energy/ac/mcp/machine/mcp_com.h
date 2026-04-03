f9a46594d2a3862d7ebdf2caad51776fa04f9b17
Arne Tarara <arne@datafuse.de>
2026-04-03 17:40:53 +0200

    Mcp granularity (#1630)
    * Switching MCP granularity up by factor of 8x
    
    * Normalized outputs to uW and uJ

--- metric_providers/psu/energy/ac/mcp/machine/mcp_com.h
+++ metric_providers/psu/energy/ac/mcp/machine/mcp_com.h
@@ -7,9 +7,9 @@
 enum mcp_types { f501, f511 };
 
 int f511_init(const char *port, bool enable_energy);
-/* Power in 10mW for channel 1 and 2 */
-int f511_get_power(int *ch1, int *ch2, int fd);
-/* Import-active energy counter for channel 1 and 2 */
+/* Power in raw MCP units for channel 1 and 2 */
+int f511_get_power(uint32_t *ch1, uint32_t *ch2, int fd);
+/* Import-active energy counter in raw MCP units for channel 1 and 2 */
 int f511_get_energy(uint64_t *ch1, uint64_t *ch2, int fd);
 int f511_set_energy_counting(int fd, bool enable_energy);
 
