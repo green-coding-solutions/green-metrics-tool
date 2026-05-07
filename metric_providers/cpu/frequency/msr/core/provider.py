8651c87d70ede3b405a7b8c4ed72971c225d18ee
Arne Tarara <arne@datafuse.de>
2026-05-07 15:34:42 +0200

    Option to set base frequency [skip ci]

--- metric_providers/cpu/frequency/msr/core/provider.py
+++ metric_providers/cpu/frequency/msr/core/provider.py
@@ -3,7 +3,8 @@ import os
 from metric_providers.base import BaseMetricProvider
 
 class CpuFrequencyMsrCoreProvider(BaseMetricProvider):
-    def __init__(self, sampling_rate, folder, skip_check=False):
+    def __init__(self, sampling_rate, folder, base_ghz=2.4, skip_check=False):
+        self._base_ghz = base_ghz
         super().__init__(
             metric_name='cpu_frequency_msr_core',
             metrics={'time': int, 'value': int, 'core_id': int},
@@ -13,6 +14,9 @@ class CpuFrequencyMsrCoreProvider(BaseMetricProvider):
             skip_check=skip_check,
             folder=folder,
         )
+
+    def _add_extra_switches(self, call_string):
+        return f"{call_string} -f {self._base_ghz}"
 
     def _parse_metrics(self, df):
 
