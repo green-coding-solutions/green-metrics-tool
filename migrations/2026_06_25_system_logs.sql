782e08a233fd567e5ce0cc314aaf6be2980e5879
Arne Tarara <arne@datafuse.de>
2026-06-25 11:17:05 +0200

    Forgot to add migration

---
+++ migrations/2026_06_25_system_logs.sql
@@ -0,0 +1,22 @@
+CREATE TABLE system_logs (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title text NOT NULL,
+    message text NOT NULL,
+    level text NOT NULL DEFAULT 'error',
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone
+);
+
+CREATE TRIGGER system_logs_moddatetime
+    BEFORE UPDATE ON system_logs
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (updated_at);
+
+UPDATE users
+SET capabilities = jsonb_set(
+    capabilities,
+    '{api,routes}',
+    (capabilities->'api'->'routes') || '"/v1/system-logs"'::jsonb || '"/v1/system-log"'::jsonb
+)
+WHERE id = 1
+    AND NOT (capabilities->'api'->'routes' ? '/v1/system-logs');
