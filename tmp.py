04cb6ffa1231c6ca3191fd12c836096cea8f80d4
MaximilianJahns <jahns@htw-berlin.de>
2026-07-30 07:44:38 +0200

    Delete tmp.py

--- tmp.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-con = psycopg2.connect("dbname=green_coding user=postgres")
-cur = con.cursor()
-cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public' ORDER BY table_name")
-for row in cur.fetchall(): print(row[0])
-con.close()
