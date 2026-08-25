1e396dc50a0d67b40807390018f27eebfdd99450
Didi Hoffmann <didi@ribalba.de>
2026-08-25 15:36:38 +0200

    Removes the timeout and relies on the defaults

--- templates/partials/gmt-playwright-ipc.js
+++ templates/partials/gmt-playwright-ipc.js
@@ -2,8 +2,6 @@ const { firefox, chromium } = require('playwright');
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-const DEFAULT_TIMEOUT = 3000; // ms
-
 // global variables, since we want to keep function signatures slim for exposed functions in usage_scenario.yml
 let browser = null;
 let context = null;
@@ -26,8 +24,6 @@ function sleep(ms) {
 async function gmtPlaywrightResetContext() {
     await context.close();
     context = await browser.newContext(contextOptions);
-    context.setDefaultTimeout(DEFAULT_TIMEOUT);
-    context.setDefaultNavigationTimeout(DEFAULT_TIMEOUT);
     page = await context.newPage();
 }
 
@@ -80,9 +76,6 @@ async function run(browserName, headless, proxy) {
   }
 
   context = await browser.newContext(contextOptions);
-  context.setDefaultTimeout(DEFAULT_TIMEOUT);
-  context.setDefaultNavigationTimeout(DEFAULT_TIMEOUT);
-
   await context.clearCookies();
   page = await context.newPage()
 
